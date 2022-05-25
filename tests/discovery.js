128d5086a80bcd5fd7d08bda9592d485d8db0b16
sethvincent <sethvincent@gmail.com>
2022-05-24 17:43:18 -0700

    add tsconfig, wip revisions to discovery.js

--- tests/discovery.js
+++ tests/discovery.js
@@ -5,7 +5,7 @@ import { keyPair } from 'hypercore-crypto'
 
 import { Discovery } from '../lib/discovery.js'
 
-test('mdns', async (t) => {
+test.skip('mdns', async (t) => {
 	t.plan(2)
 
 	const discovery1 = new Discovery({ keyPair: keyPair(), dht: false })
@@ -30,7 +30,7 @@ test('mdns', async (t) => {
 	})
 })
 
-test('dht', async (t) => {
+test.skip('dht', async (t) => {
 	t.plan(2)
 
 	const testnet = await createTestnet(10)
@@ -53,6 +53,37 @@ test('dht', async (t) => {
 			await discovery1.destroy()
 			await discovery2.destroy()
 			await testnet.destroy()
+		})
+	})
+
+	await discovery1.announce(topic)
+	await discovery2.lookup(topic)
+})
+
+test('dedupe peers', async (t) => {
+	t.plan(4)
+
+	const testnet = await createTestnet(10)
+	const bootstrap = testnet.bootstrap
+
+	const discovery1 = new Discovery({ keyPair: keyPair(), dht: { bootstrap } })
+	const discovery2 = new Discovery({ keyPair: keyPair(), dht: { bootstrap } })
+
+	const topic = Buffer.alloc(32).fill('mapeo-example')
+
+	discovery1.on('connection', async (connection, info) => {
+		console.log('discovery1 connection', connection.publicKey.toString('hex'), info.connectionType)
+		connection.write(Buffer.from('hello'))
+	})
+
+	discovery2.on('connection', async (connection, info) => {
+		t.ok(info)
+
+		connection.on('data', async (data) => {
+			t.ok(data.toString() === 'hello')
+			// await discovery1.destroy()
+			// await discovery2.destroy()
+			// await testnet.destroy()
 		})
 	})
 
