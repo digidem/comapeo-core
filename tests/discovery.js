c933bfef422ab02415b00ae68744a131e92b7f3e
sethvincent <sethvincent@gmail.com>
2022-07-07 19:11:02 -0700

    refactor to better handle topic and peer state across discovery types

--- tests/discovery.js
+++ tests/discovery.js
@@ -1,13 +1,13 @@
-import log from 'why-is-node-running'
 import test from 'brittle'
 
+import Hypercore from 'hypercore'
+import ram from 'random-access-memory'
 import createTestnet from '@hyperswarm/testnet'
 
 import { createCoreKeyPair, createIdentityKeys } from './helpers/index.js'
 import { Discovery } from '../lib/discovery.js'
 
-// TODO: figure out why hyperswarm hangs the tests
-test.skip('discovery - dht/hyperswarm', async (t) => {
+test('discovery - dht/hyperswarm', async (t) => {
   t.plan(2)
 
   const testnet = await createTestnet(10)
@@ -22,23 +22,26 @@ test.skip('discovery - dht/hyperswarm', async (t) => {
   const identityPublicKey2 = identity2.identityKeyPair.publicKey.toString('hex')
 
   const discover1 = new Discovery({
-    keyPair: identity1.identityKeyPair,
+    identityKeyPair: identity1.identityKeyPair,
     mdns: false,
     dht: { bootstrap, server: true, client: true },
   })
 
   const discover2 = new Discovery({
-    keyPair: identity2.identityKeyPair,
+    identityKeyPair: identity2.identityKeyPair,
     mdns: false,
     dht: { bootstrap, server: true, client: true },
   })
 
-  discover1.on('peer', async (connection, peer) => {
+  await discover1.ready()
+  await discover2.ready()
+
+  discover1.on('connection', async (connection, peer) => {
     t.ok(peer.identityPublicKey === identityPublicKey2, 'match key of 2nd peer')
     await step()
   })
 
-  discover2.on('peer', async (connection, peer) => {
+  discover2.on('connection', async (connection, peer) => {
     t.ok(peer.identityPublicKey === identityPublicKey1, 'match key of 1st peer')
     await step()
   })
@@ -49,20 +52,29 @@ test.skip('discovery - dht/hyperswarm', async (t) => {
     if (count === 2) {
       await discover1.leave(keyPair.publicKey)
       await discover2.leave(keyPair.publicKey)
+      await discover1.destroy()
+      await discover2.destroy()
       await testnet.destroy()
-      log()
     }
   }
 
-  // TODO: use discoveryKey
   await discover1.join(keyPair.publicKey)
   await discover2.join(keyPair.publicKey)
 })
 
-test('discovery - mdns', async (t) => {
-  t.plan(2)
+test('replication - dht/hyperswarm', async (t) => {
+  t.plan(3)
 
-  const keyPair = createCoreKeyPair('dht-peer-discovery')
+  const testnet = await createTestnet(10)
+  const bootstrap = testnet.bootstrap
+
+  const core1 = new Hypercore(ram)
+  await core1.ready()
+
+  const core2 = new Hypercore(ram, core1.key)
+  await core2.ready()
+
+  core1.append(['a', 'b', 'c'])
 
   const identity1 = createIdentityKeys()
   const identityPublicKey1 = identity1.identityKeyPair.publicKey.toString('hex')
@@ -71,23 +83,90 @@ test('discovery - mdns', async (t) => {
   const identityPublicKey2 = identity2.identityKeyPair.publicKey.toString('hex')
 
   const discover1 = new Discovery({
-    keyPair: identity1.identityKeyPair,
+    identityKeyPair: identity1.identityKeyPair,
+    mdns: false,
+    dht: { bootstrap, server: true, client: true },
+  })
+
+  const discover2 = new Discovery({
+    identityKeyPair: identity2.identityKeyPair,
+    mdns: false,
+    dht: { bootstrap, server: true, client: true },
+  })
+
+  await discover1.ready()
+  await discover2.ready()
+
+  discover1.on('connection', async (connection, peer) => {
+    t.ok(peer.identityPublicKey === identityPublicKey2, 'match key of 2nd peer')
+    core1.replicate(connection)
+    await step()
+  })
+
+  discover2.on('connection', async (connection, peer) => {
+    t.ok(peer.identityPublicKey === identityPublicKey1, 'match key of 1st peer')
+    core2.replicate(connection)
+
+    const stream = core2.createReadStream()
+
+    const results = []
+    stream.on('data', async (/** @type {any} */ data) => {
+      results.push(data)
+      if (results.length === 3) {
+        t.pass()
+        await step()
+      }
+    })
+  })
+
+  let count = 0
+  async function step() {
+    count++
+    if (count === 2) {
+      await discover1.leave(core1.discoveryKey)
+      await discover2.leave(core1.discoveryKey)
+      await discover1.destroy()
+      await discover2.destroy()
+      await testnet.destroy()
+    }
+  }
+
+  await discover1.join(core1.discoveryKey)
+  await discover2.join(core1.discoveryKey)
+})
+
+test('discovery - mdns', async (t) => {
+  t.plan(2)
+
+  const keyPair = createCoreKeyPair('mdns-peer-discovery')
+
+  const identity1 = createIdentityKeys()
+  const identityPublicKey1 = identity1.identityKeyPair.publicKey.toString('hex')
+
+  const identity2 = createIdentityKeys()
+  const identityPublicKey2 = identity2.identityKeyPair.publicKey.toString('hex')
+
+  const discover1 = new Discovery({
+    identityKeyPair: identity1.identityKeyPair,
     mdns: true,
     dht: false,
   })
 
   const discover2 = new Discovery({
-    keyPair: identity2.identityKeyPair,
+    identityKeyPair: identity2.identityKeyPair,
     mdns: true,
     dht: false,
   })
 
-  discover1.on('peer', async (connection, peer) => {
+  await discover1.ready()
+  await discover2.ready()
+
+  discover1.on('connection', async (connection, peer) => {
     t.ok(peer.identityPublicKey === identityPublicKey2, 'match key of 2nd peer')
     await step()
   })
 
-  discover2.on('peer', async (connection, peer) => {
+  discover2.on('connection', async (connection, peer) => {
     t.ok(peer.identityPublicKey === identityPublicKey1, 'match key of 1st peer')
     await step()
   })
@@ -103,65 +182,70 @@ test('discovery - mdns', async (t) => {
     }
   }
 
-  // TODO: use discoveryKey
   await discover1.join(keyPair.publicKey)
   await discover2.join(keyPair.publicKey)
 })
 
 test('discovery - multiple successive joins', async (t) => {
-  t.plan(3)
+  t.plan(4)
 
-  const keyPair = createCoreKeyPair('dht-peer-discovery')
+  const keyPair = createCoreKeyPair('multiple successive joins')
   const identity1 = createIdentityKeys()
 
   const discover1 = new Discovery({
-    keyPair: identity1.identityKeyPair,
+    identityKeyPair: identity1.identityKeyPair,
     mdns: true,
     dht: true,
   })
 
+  await discover1.ready()
+
   await discover1.join(keyPair.publicKey)
-  t.ok(discover1.discover.length === 1)
+  t.ok(discover1.topics.length === 1)
   await discover1.join(keyPair.publicKey)
-  t.ok(discover1.discover.length === 1)
+
+  t.ok(discover1.topics.length === 1)
   await discover1.join(keyPair.publicKey)
-  t.ok(discover1.discover.length === 1)
+
+  t.ok(discover1.topics.length === 1)
 
   await discover1.leave(keyPair.publicKey)
   await discover1.destroy()
+  t.ok(discover1.topics.length === 0)
 })
 
-// TODO: this test helps to show race condition problems with successive joining and leaving
-test.skip('discovery - mdns join, leave, join', async (t) => {
+test('discovery - mdns join, leave, join', async (t) => {
   t.plan(5)
 
-  const keyPair = createCoreKeyPair('dht-peer-discovery')
+  const keyPair = createCoreKeyPair('join, leave, join')
   const identity1 = createIdentityKeys()
   const identityPublicKey1 = identity1.identityKeyPair.publicKey.toString('hex')
 
   const identity2 = createIdentityKeys()
 
   const discover1 = new Discovery({
-    keyPair: identity1.identityKeyPair,
+    identityKeyPair: identity1.identityKeyPair,
     mdns: true,
     dht: false,
   })
-
   const discover2 = new Discovery({
-    keyPair: identity2.identityKeyPair,
+    identityKeyPair: identity2.identityKeyPair,
     mdns: true,
     dht: false,
   })
 
-  discover2.on('peer', async (connection, peer) => {
+  await discover1.ready()
+  await discover2.ready()
+
+  discover2.on('connection', async (connection, peer) => {
     t.ok(peer.identityPublicKey === identityPublicKey1, 'match key of 1st peer')
     t.ok(discover2.peers.length === 1, 'expected peers count')
 
     await discover1.leave(keyPair.publicKey)
-    t.ok(discover1.discover.length === 0, 'no discover objects')
+    t.ok(discover1.topics.length === 0, 'no topics')
 
     await discover1.join(keyPair.publicKey)
-    t.ok(discover1.discover.length === 1, 'expected discover object')
+    t.ok(discover1.topics.length === 1, 'expected topic object')
 
     await discover1.leave(keyPair.publicKey)
     await discover2.leave(keyPair.publicKey)
@@ -170,6 +254,6 @@ test.skip('discovery - mdns join, leave, join', async (t) => {
   })
 
   await discover1.join(keyPair.publicKey)
-  t.ok(discover1.discover.length === 1, 'expected discover object')
+  t.ok(discover1.topics.length === 1, 'expected topic object')
   await discover2.join(keyPair.publicKey)
 })
---
+++ types/modules.d.ts
@@ -0,0 +1,8 @@
+// TODO: create types for these modules
+declare module 'hyperswarm'
+declare module '@hyperswarm/secret-stream'
+declare module '@hyperswarm/testnet'
+declare module 'base32.js'
+declare module '@mapeo/crypto'
+declare module 'hypercore'
+declare module 'random-access-memory'
