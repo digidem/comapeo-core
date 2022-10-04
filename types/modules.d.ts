f70d2119bd013d226f3405144320603a1b13ea74
Andrew Chou <andrewchou@fastmail.com>
2022-10-04 15:52:25 -0400

    chore: add types for @hyperswarm/secret-stream

--- types/modules.d.ts
+++ types/modules.d.ts
@@ -1,6 +1,64 @@
 // TODO: create types for these modules
 declare module 'hyperswarm'
-declare module '@hyperswarm/secret-stream'
+declare module '@hyperswarm/secret-stream' {
+  import { Duplex as NodeDuplex } from 'stream'
+  import { Duplex, DuplexEvents } from 'streamx'
+
+  interface Opts {
+    autostart?: boolean
+    // TODO: Use https://github.com/chm-diederichs/noise-handshake/blob/main/noise.js for specific patterns
+    pattern?: string
+    remotePublicKey?: Buffer
+    keyPair?: { publicKey: Buffer; secretKey: Buffer }
+    handshake?: {
+      tx: Buffer
+      rx: Buffer
+      hash: Buffer
+      publicKey: Buffer
+      remotePublicKey: Buffer
+    }
+  }
+
+  type NoiseStreamEvents = {
+    connect: () => void
+  }
+
+  class SecretStream<
+    RawStream extends NodeDuplex | Duplex = Duplex
+  > extends Duplex<
+    any,
+    any,
+    any,
+    any,
+    true,
+    true,
+    DuplexEvents<any, any> & NoiseStreamEvents
+  > {
+    public publicKey: Buffer
+    public remotePublicKey: Buffer
+    public handshakeHash: Buffer
+    public rawStream: RawStream
+    public isInitiator: boolean
+    public noiseStream: this
+    public opened: Promise<boolean>
+    public userData: any
+
+    constructor(isInitiator: boolean, rawStream?: RawStream, opts?: Opts)
+
+    static keyPair(seed?: Buffer): {
+      publicKey: Buffer
+      secretKey: Buffer
+    }
+
+    public start(rawStream?: NodeDuplex, opts?: Opts): void
+
+    public setTimeout(ms?: number): void
+
+    public setKeepAlive(ms?: number): void
+  }
+
+  export = SecretStream
+}
 declare module '@hyperswarm/testnet'
 declare module 'base32.js'
 declare module '@mapeo/crypto'
