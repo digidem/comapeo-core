2d37e86c01d94f1d9dc7034698c78ede11bf1ddb
Andrew Chou <andrewchou@fastmail.com>
2022-10-06 11:27:43 -0400

    chore: add types for random-access-memory (#35)

--- types/modules.d.ts
+++ types/modules.d.ts
@@ -62,7 +62,89 @@ declare module 'base32.js'
 declare module '@mapeo/crypto'
 declare module 'hypercore'
 declare module 'corestore'
-declare module 'random-access-memory'
+declare module 'random-access-storage' {
+  import EventEmitter from 'events'
+
+  type Cb<T> = (err: any, val?: T) => void
+
+  class Request {
+    readonly type: number
+    readonly offset: number
+    readonly size: number
+    readonly data: Buffer
+    readonly storage: RandomAccessStorage
+
+    constructor(
+      self: RandomAccessStorage,
+      type: number,
+      offset: number,
+      size: number,
+      data: Buffer,
+      cb: Cb<any>
+    )
+
+    callback: Cb<any>
+  }
+
+  class RandomAccessStorage extends EventEmitter {
+    readonly opened: boolean
+    readonly suspended: boolean
+    readonly closed: boolean
+    readonly unlinked: boolean
+    readonly writing: boolean
+    readonly readable: boolean
+    readonly writable: boolean
+    readonly deletable: boolean
+    readonly truncatable: boolean
+    readonly statable: boolean
+
+    constructor(opts?: {
+      open?: boolean
+      read?: boolean
+      write?: boolean
+      del?: boolean
+      truncate?: boolean
+      stat?: boolean
+      suspend?: boolean
+      close?: boolean
+      unlink?: boolean
+    })
+
+    read(offset: number, size: number, cb: Cb<any>): void
+    write(offset: number, data: Buffer, cb?: Cb<any>): void
+    del(offset: number, size: number, cb?: Cb<any>): void
+    truncate(offset: number, cb?: Cb<any>): void
+    stat(cb: Cb<any>): void
+    open(cb?: Cb<any>): void
+    suspend(cb?: Cb<any>): void
+    close(cb?: Cb<any>): void
+    unlink(cb?: Cb<any>): void
+    run(req: Request, writing?: boolean): void
+  }
+
+  export = RandomAccessStorage
+}
+declare module 'random-access-memory' {
+  import RandomAccessStorage from 'random-access-storage'
+
+  class RandomAccessMemory extends RandomAccessStorage {
+    readonly length: number
+    readonly pageSize: number
+    readonly buffers: Buffer[]
+
+    constructor(
+      opts?:
+        | number
+        | Buffer
+        | { length?: number; buffer?: Buffer; pageSize?: number }
+    )
+
+    toBuffer(): Buffer
+    clone(): RandomAccessMemory
+  }
+
+  export = RandomAccessMemory
+}
 declare module 'random-access-file'
 declare module 'randombytes'
 declare module 'b4a'
