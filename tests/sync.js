fb5ed4b86e246417a58c16b8c41a50ca88d88e2d
Tomás Ciccola <tciccola@digital-democracy.com>
2023-01-10 17:11:39 -0300

    feat: started added tests for coreIdCache

--- tests/sync.js
+++ tests/sync.js
@@ -1,5 +1,30 @@
 import test from 'brittle'
+import { randomBytes } from 'crypto'
+import { Sqlite } from '../lib/sqlite.js'
+import { CoreIdCache } from '../sync/CoreIdCache.js'
 
-test.skip('sync - todo', async (t) => {
-  t.pass('TODO')
+const nRecords = 10
+const sqlite = new Sqlite()
+const coreIdCache = new CoreIdCache(sqlite)
+const identityId = randomBytes(32).toString('hex')
+const namespace = 'auth'
+const coreIds = new Array(nRecords).fill(null).map(_ => randomBytes(32).toString('hex'))
+
+coreIds.forEach(coreId => coreIdCache.put({namespace, coreId, identityId}))
+
+test(`CoreIdCache - put ${nRecords} cores and test if they are cached`, t => {
+  const cache = coreIdCache.getByStoreNamespace(namespace)
+  t.plan(nRecords)
+  cache[0].coreIds.forEach(coreId => {
+    if(coreIds.find(id => coreId === id)){
+      t.pass()
+    }else{
+      t.fail()
+    }
+  })
 })
+
+
+// test.skip('sync - todo', async (t) => {
+//   t.pass('TODO')
+// })
