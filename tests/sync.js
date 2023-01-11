f3d0afab4e834562023812b74ccf5f462f7f2db9
Tomás Ciccola <tciccola@digital-democracy.com>
2023-01-11 13:47:43 -0300

    chore: type errors on test/sync.js

--- tests/sync.js
+++ tests/sync.js
@@ -1,11 +1,13 @@
+//@ts-check
 import test from 'brittle'
 import { randomBytes } from 'crypto'
 import { Sqlite } from '../lib/sqlite.js'
 import { CoreIdCache } from '../sync/CoreIdCache.js'
 
-const nRecords = 10
-const sqlite = new Sqlite()
+const sqlite = new Sqlite('/tmp/table')
 const coreIdCache = new CoreIdCache(sqlite)
+
+const nRecords = 10
 const identityId = randomBytes(32).toString('hex')
 const namespace = 'auth'
 const coreIds = new Array(nRecords).fill(null).map(_ => randomBytes(32).toString('hex'))
@@ -15,7 +17,9 @@ coreIds.forEach(coreId => coreIdCache.put({namespace, coreId, identityId}))
 test(`CoreIdCache - put ${nRecords} cores and test if they are cached`, t => {
   const cache = coreIdCache.getByStoreNamespace(namespace)
   t.plan(nRecords)
-  cache[0].coreIds.forEach(coreId => {
+  cache[0].coreIds.forEach(
+    /** @param {String} coreId */
+    coreId => {
     if(coreIds.find(id => coreId === id)){
       t.pass()
     }else{
@@ -23,7 +27,6 @@ test(`CoreIdCache - put ${nRecords} cores and test if they are cached`, t => {
     }
   })
 })
-
 
 // test.skip('sync - todo', async (t) => {
 //   t.pass('TODO')
