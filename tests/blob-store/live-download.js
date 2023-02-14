9b66064e77d45487c175aebb19ab11e4a6207831
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-02-14 12:00:53 +0000

    More comprehensive test for live download

--- tests/blob-store/live-download.js
+++ tests/blob-store/live-download.js
@@ -4,28 +4,45 @@ import Hyperdrive from 'hyperdrive'
 import Corestore from 'corestore'
 import RAM from 'random-access-memory'
 import test from 'brittle'
+import { setTimeout } from 'timers/promises'
+import { once } from 'events'
+import { randomBytes } from 'node:crypto'
+
+// Test with buffers that are 3 times the default blockSize for hyperblobs
+const TEST_BUF_SIZE = 3 * 64 * 1024
 
 test('live download', async t => {
-  const store1 = new Corestore(RAM)
-  const store2 = new Corestore(RAM)
-  const drive1 = new Hyperdrive(store1)
-  await drive1.ready()
-  const drive2 = new Hyperdrive(store2, drive1.key)
+  const { drive1, drive2, replicate } = await testEnv()
 
-  await drive1.put('/foo/greeting', Buffer.from('hello'))
+  await drive1.put('/foo', randomBytes(TEST_BUF_SIZE))
+  const {
+    value: { blob: blob1 }
+  } = await drive1.entry('/foo')
 
-  const s = store1.replicate(true)
-  s.pipe(store2.replicate(false)).pipe(s)
+  const stream = replicate()
+  const blobCore2 = (await drive2.getBlobs()).core
 
   const download = new LiveDownload(drive2)
+  await waitForState(download, 'downloaded')
+  // Can't use `drive2.get()` here because connected to replication stream, so
+  // it would download anyway (no `waitFor = false` support for Hyperdrive yet)
+  t.ok(
+    await blobCore2.has(
+      blob1.blockOffset,
+      blob1.blockOffset + blob1.blockLength
+    ),
+    'First blob is downloaded'
+  )
+  t.ok(blob1.blockLength > 1, 'Blob is more than one block length')
 
-  download.on('state', console.log)
+  const expected = randomBytes(TEST_BUF_SIZE)
+  await drive1.put('/bar', expected)
 
   await waitForState(download, 'downloaded')
+  stream.destroy()
+  await once(stream, 'close')
 
-  await drive1.put('/foo/leaving', Buffer.from('goodbye'))
-
-  await waitForState(download, 'downloaded')
+  t.alike(await drive2.get('/bar'), expected, 'Second blob is downloaded')
 })
 
 /** @returns {Promise<void>} */
@@ -37,4 +54,25 @@ async function waitForState (download, status) {
       res()
     })
   })
+}
+
+async function testEnv () {
+  const store1 = new Corestore(RAM)
+  const store2 = new Corestore(RAM)
+  const drive1 = new Hyperdrive(store1)
+  await drive1.ready()
+  const drive2 = new Hyperdrive(store2, drive1.key)
+  await drive2.ready()
+
+  function replicate () {
+    const s = store1.replicate(true)
+    s.pipe(store2.replicate(false)).pipe(s)
+    return s
+  }
+
+  return {
+    drive1,
+    drive2,
+    replicate
+  }
 }
