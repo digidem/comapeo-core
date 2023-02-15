58c6a5991ddd3b031b26955056bf13d188634725
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-02-15 18:39:51 +0000

    Additional tests for live download

--- tests/blob-store/live-download.js
+++ tests/blob-store/live-download.js
@@ -4,8 +4,8 @@ import Hyperdrive from 'hyperdrive'
 import Corestore from 'corestore'
 import RAM from 'random-access-memory'
 import test from 'brittle'
-import { setTimeout } from 'timers/promises'
-import { once } from 'events'
+import { setTimeout } from 'node:timers/promises'
+import { once } from 'node:events'
 import { randomBytes } from 'node:crypto'
 
 // Test with buffers that are 3 times the default blockSize for hyperblobs
@@ -68,8 +68,132 @@ test('sparse live download', async t => {
 
   t.alike(await drive2.get('/foo/one'), buf1)
   t.alike(await drive2.get('/foo/two'), buf2)
-  await t.exception(() => drive2.get('/bar/one'), 'Block not available')
-  await t.exception(() => drive2.get('/bar/two'), 'Block not available')
+  await t.exception(
+    () => drive2.get('/bar/one', { wait: false }),
+    'Block not available'
+  )
+  await t.exception(
+    () => drive2.get('/bar/two', { wait: false }),
+    'Block not available'
+  )
+})
+
+test('Abort download (same tick)', async t => {
+  const { drive1, drive2, replicate } = await testEnv()
+  await drive1.put('/foo', randomBytes(TEST_BUF_SIZE))
+  const stream = replicate()
+  const controller = new AbortController()
+  const download = new LiveDownload(drive2, { signal: controller.signal })
+  controller.abort()
+  stream.destroy()
+  await once(stream, 'close')
+  t.alike(download.state, {
+    haveCount: 0,
+    haveBytes: 0,
+    wantCount: 0,
+    wantBytes: 0,
+    error: null,
+    status: 'aborted'
+  })
+  t.is(await drive2.get('/foo'), null, 'nothing downloaded')
+})
+
+test('Abort download (next event loop)', async t => {
+  const { drive1, drive2, replicate } = await testEnv()
+  await drive1.put('/one', randomBytes(TEST_BUF_SIZE))
+  const stream = replicate()
+  const controller = new AbortController()
+  const download = new LiveDownload(drive2, { signal: controller.signal })
+  // This is the only way to trigger abort before the entryStream loop
+  await drive2.getBlobs()
+  controller.abort()
+  stream.destroy()
+  await once(stream, 'close')
+  t.alike(download.state, {
+    haveCount: 0,
+    haveBytes: 0,
+    wantCount: 0,
+    wantBytes: 0,
+    error: null,
+    status: 'aborted'
+  })
+  await t.exception(
+    () => drive2.get('/foo', { wait: false }),
+    Error,
+    'Block not available'
+  )
+})
+
+test('Abort download (after initial download)', async t => {
+  const { drive1, drive2, replicate } = await testEnv()
+
+  const buf1 = randomBytes(TEST_BUF_SIZE)
+  await drive1.put('/one', buf1)
+
+  const stream = replicate()
+  const controller = new AbortController()
+  const download = new LiveDownload(drive2, { signal: controller.signal })
+  await waitForState(download, 'downloaded')
+
+  controller.abort()
+
+  await drive1.put('/two', randomBytes(TEST_BUF_SIZE))
+
+  // Nothing should happen here, but allow some time to see if it does
+  await setTimeout(200)
+
+  stream.destroy()
+  await once(stream, 'close')
+
+  t.alike(await drive2.get('/one'), buf1, 'First blob is downloaded')
+  await t.exception(
+    () => drive2.get('/two', { wait: false }),
+    Error,
+    'Second blob is not downloaded'
+  )
+})
+
+test('Live download when data is already downloaded', async t => {
+  const { drive1, drive2, replicate } = await testEnv()
+
+  const buf1 = randomBytes(TEST_BUF_SIZE)
+  await drive1.put('/one', buf1)
+
+  const stream1 = replicate()
+
+  await drive2.download()
+  t.alike(await drive2.get('/one'), buf1, 'First blob is downloaded')
+
+  stream1.destroy()
+  await once(stream1, 'close')
+
+  const stream2 = replicate()
+  const download = new LiveDownload(drive2)
+  await waitForState(download, 'downloaded')
+  t.alike(download.state, {
+    haveCount: 1,
+    haveBytes: buf1.byteLength,
+    wantCount: 0,
+    wantBytes: 0,
+    error: null,
+    status: 'downloaded'
+  }, 'Blob already downloaded is included in state')
+
+  const buf2 = randomBytes(TEST_BUF_SIZE)
+  await drive1.put('/two', buf2)
+  await waitForState(download, 'downloaded')
+
+  stream2.destroy()
+  await once(stream2, 'close')
+
+  t.alike(await drive2.get('/two'), buf2, 'Second blob is downloaded')
+})
+
+test('Initial status', async t => {
+  const { drive1 } = await testEnv()
+
+  const download = new LiveDownload(drive1)
+  t.is(download.state.status, 'checking', 'initial status is \'checking\'')
 })
 
 /** @returns {Promise<void>} */
