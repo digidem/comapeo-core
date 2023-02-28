d58dfe8d8a0ded3fb7ac6a85dd9c0c75b3189cf6
sethvincent <sethvincent@gmail.com>
2023-02-27 17:52:02 -0800

    initial sketching of mapeo project api

--- index.js
+++ index.js
@@ -1,34 +1,105 @@
-export class Mapeo {
-  #corestore
+import { KeyManager } from '@mapeo/crypto'
+
+import { AuthStore } from './lib/authstore/index.js'
+import { DataStore } from './lib/datastore/index.js'
+import { Discovery } from './lib/discovery/index.js'
+import { CoreManager } from './lib/core-manager/index.js'
+import { Sqlite } from './lib/sqlite.js'
+
+export class MapeoProject {
+  #sqliteDatabase
   #sqlite
 
+  #authstore
+  #datastore
+  #discovery
+  #coreManager
+  #keyManager
+
+  #rootKey
+  #identityKeyPair
+  #projectPublicKey
+  #projectSecretKey
+
   /**
-   *
-   * @param {Object} options
-   * @param {import('corestore')} options.corestore
-   * @param {import('./lib/sqlite').Sqlite} options.sqlite
+   * @typedef {Object} MapeoProjectOptionsBase
+   * @property {Buffer} rootKey
+   * @property {any} storage
+   * @property {string} sqliteFilepath
+   * @property {IdentityKeyPair} identityKeyPair
+   * @property {Boolean | undefined} mdns
+   * @property {Boolean | DhtOptions | undefined} dht
+   * 
+   * @typedef {MapeoProjectOptionsBase & { projectPublicKey: PublicKey, projectKeyPair: never }} MapeoProjectPublicKeyOption
+   * @typedef {MapeoProjectOptionsBase & { projectPublicKey: never, projectKeyPair: KeyPair }} MapeoProjectKeyPairOption
+   * @typedef {MapeoProjectPublicKeyOption | MapeoProjectKeyPairOption} MapeoProjectOptions
+   */
+
+  /**
+   * @param {MapeoProjectOptions} options
    */
   constructor(options) {
-    const { corestore, sqlite } = options
-    this.#corestore = corestore
-    this.#sqlite = sqlite
-  }
+    this.storage = options.storage
+    this.#rootKey = options.rootKey
+    this.#keyManager = new KeyManager(this.#rootKey)
+    this.#identityKeyPair = options.identityKeyPair
 
-  async ready() {}
+    // Internal sqlite wrapper
+    this.#sqlite = new Sqlite(options.sqliteFilepath)
 
-  get keys() {
-    return this.cores.map((core) => {
-      return core.key.toString('hex')
+    // Access to the sqlite database
+    this.#sqliteDatabase = this.#sqlite.db
+
+    if (options.projectPublicKey) {
+      this.#projectPublicKey = options.projectPublicKey
+    } else if (options.projectKeyPair) {
+      this.#projectPublicKey = options.projectKeyPair.publicKey
+      this.#projectSecretKey = options.projectKeyPair.secretKey
+    } else {
+      throw new Error('Must provide either projectPublicKey or projectKeyPair')
+    }
+
+    this.#discovery = new Discovery({
+      identityKeyPair: this.#identityKeyPair,
+      mdns: options.mdns,
+      dht: options.dht,
+    })
+
+    this.#coreManager = new CoreManager({
+      keyManager: this.#keyManager,
+      storage: this.storage,
+      db: this.#sqliteDatabase,
+      projectKey: this.#projectPublicKey,
+      projectSecretKey: this.#projectSecretKey,
+    })
+
+    this.#authstore = new AuthStore({
+      identityKeyPair: this.#identityKeyPair,
+      projectPublicKey: this.#projectPublicKey, // TODO: handle pubkey vs keypair in authstore like MapeoProjectOptions
+      keyPair: options.projectKeyPair,
+      corestore: this.#corestore, // TODO: replace with coreManager
+      sqlite: this.#sqlite,
+    })
+
+    this.#datastore = new DataStore({
+      dataTypes: [{ // TODO: replace with real schemas
+        name: 'observation',
+        schema: {},
+        blockPrefix: 'o',
+        extraColumns: ''
+      }],
+      identityPublicKey: this.#identityKeyPair.publicKey,
+      keyPair: options.projectKeyPair,
+      corestore: this.#corestore, // TODO: replace with coreManager
+      sqlite: this.#sqlite,
     })
   }
 
-  get cores() {
-    return [...this.#corestore.cores.values()]
+  async ready() {
+    await this.#authstore.ready()
   }
 
-  async sync() {}
-
-  async syncAuthStore() {}
-
-  async syncDataStores() {}
+  async close() {
+    await this.#authstore.close()
+  }
 }
