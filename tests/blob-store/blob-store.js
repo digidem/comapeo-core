481c99130e164a9bed6bb4c99964dcb93fa6ffb4
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-03-01 22:10:27 +0000

    tests for get() when blob is not found

--- tests/blob-store/blob-store.js
+++ tests/blob-store/blob-store.js
@@ -24,6 +24,94 @@ test('blobStore.put(blobId, buf) and blobStore.get(blobId)', async t => {
   t.alike(bndlbuf, diskbuf, 'should be equal')
 })
 
+test('get(), driveId not found', async t => {
+  const { blobStore } = await testenv()
+  await t.exception(async () => await blobStore.get({
+    type: 'photo',
+    variant : 'original',
+    name: 'test-file',
+    driveId: randomBytes(32).toString('hex')
+  }))
+})
+
+test('get(), valid driveId, missing file', async t => {
+  const { blobStore, coreManager } = await testenv()
+  const driveId = coreManager.getWriterCore('blobIndex').key.toString('hex')
+  await t.exception(async () => await blobStore.get({
+    type: 'photo',
+    variant : 'original',
+    name: 'test-file',
+    driveId
+  }))
+})
+
+test('get(), uninitialized drive', async t => {
+  const { blobStore, coreManager } = await testenv()
+  const driveKey = randomBytes(32)
+  const driveId = driveKey.toString('hex')
+  coreManager.addCore(driveKey, 'blobIndex')
+  await t.exception(async () => await blobStore.get({
+    type: 'photo',
+    variant : 'original',
+    name: 'test-file',
+    driveId
+  }))
+})
+
+test('get(), initialized but unreplicated drive', async t => {
+  const projectKey = randomBytes(32)
+  const { blobStore: bs1, coreManager: cm1 } = await testenv({ projectKey })
+  const { blobStore: bs2, coreManager: cm2 } = await testenv({ projectKey })
+
+  const blob1 = randomBytes(TEST_BUF_SIZE)
+  const blob1Id = /** @type {const} */ ({
+    type: 'photo',
+    variant: 'original',
+    name: 'blob1'
+  })
+  const driveId = await bs1.put(blob1Id, blob1)
+
+  const { destroy } = replicateBlobs(cm1, cm2)
+  /** @type {any} */
+  const replicatedCore = cm2.getCoreByKey(Buffer.from(driveId, 'hex'))
+  await replicatedCore.update()
+  await destroy()
+  t.is(replicatedCore.contiguousLength, 0, 'data is not downloaded')
+  t.ok(replicatedCore.length > 0, 'proof of length has updated')
+  await t.exception(async () => await bs2.get({
+    ...blob1Id,
+    driveId
+  }))
+})
+
+test('get(), replicated blobIndex, but blobs not replicated', async t => {
+  const projectKey = randomBytes(32)
+  const { blobStore: bs1, coreManager: cm1 } = await testenv({ projectKey })
+  const { blobStore: bs2, coreManager: cm2 } = await testenv({ projectKey })
+
+  const blob1 = randomBytes(TEST_BUF_SIZE)
+  const blob1Id = /** @type {const} */ ({
+    type: 'photo',
+    variant: 'original',
+    name: 'blob1'
+  })
+  const driveId = await bs1.put(blob1Id, blob1)
+
+  const { destroy } = replicateBlobs(cm1, cm2)
+  /** @type {any} */
+  const replicatedCore = cm2.getCoreByKey(Buffer.from(driveId, 'hex'))
+  await replicatedCore.update()
+  await replicatedCore.download({ end: replicatedCore.length }).done()
+  await destroy()
+
+  t.is(replicatedCore.contiguousLength, replicatedCore.length, 'blobIndex has downloaded')
+  t.ok(replicatedCore.length > 0)
+  await t.exception(async () => await bs2.get({
+    ...blob1Id,
+    driveId
+  }))
+})
+
 test('blobStore.createWriteStream(blobId) and blobStore.createReadStream(blobId)', async t => {
   const { blobStore } = await testenv()
   const diskbuf = await readFile(new URL(import.meta.url))
@@ -35,17 +123,7 @@ test('blobStore.createWriteStream(blobId) and blobStore.createReadStream(blobId)
   const ws = blobStore.createWriteStream(blobId)
   const { driveId } = ws
   await pipeline(fs.createReadStream(new URL(import.meta.url)), ws)
-  let bndlbuf = null
-  await pipeline(
-    blobStore.createReadStream({ ...blobId, driveId }),
-    new Writable({
-      write (data, cb) {
-        if (bndlbuf) bndlbuf = data.concat(bndlbuf)
-        else bndlbuf = data
-        return cb(null)
-      }
-    })
-  )
+  const bndlbuf = await concat(blobStore.createReadStream({ ...blobId, driveId }))
   t.alike(bndlbuf, diskbuf, 'should be equal')
 })
 
@@ -186,4 +264,19 @@ function replicateBlobs (cm1, cm2) {
     rsm: /** @type {const} */ ([rsm1, rsm2]),
     destroy
   }
+}
+
+async function concat(rs) {
+  let buf = null
+  await pipeline(
+    rs,
+    new Writable({
+      write (data, cb) {
+        if (buf) buf = data.concat(buf)
+        else buf = data
+        return cb(null)
+      }
+    })
+  )
+  return buf
 }
