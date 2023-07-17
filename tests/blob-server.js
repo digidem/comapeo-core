436d0f7d58f7994fe496d2c18135c4dc18e7576c
Andrew Chou <andrewchou@fastmail.com>
2023-07-17 16:46:39 +0100

    extract fixtures setup in tests

--- tests/blob-server.js
+++ tests/blob-server.js
@@ -16,28 +16,16 @@ const IMAGE_FIXTURES = readdirSync(IMAGE_FIXTURES_PATH)
 const SUPPORTED_IMAGE_EXTENSIONS = /** @type {const} */ (['png', 'jpg', 'jpeg'])
 
 test('Plugin handles prefix option properly', async (t) => {
-  const { blobStore, server } = await testenv()
+  const { blobStore, data, server } = await testenv()
 
   const prefix = '/blobs'
 
   server.register(BlobServerPlugin, { blobStore, prefix })
 
-  for (const fixture of IMAGE_FIXTURES) {
-    const imagePath = path.resolve(IMAGE_FIXTURES_PATH, fixture)
-    const parsedFixture = path.parse(fixture)
-    const diskbuf = await readFile(imagePath)
-
-    const blobId = /** @type {const} */ ({
-      type: 'photo',
-      variant: 'original',
-      name: parsedFixture.name,
-    })
-
-    const driveId = await blobStore.put(blobId, diskbuf)
-
+  for (const { blobId } of data) {
     const res = await server.inject({
       method: 'GET',
-      url: `${prefix}/${driveId}/${blobId.type}/${blobId.variant}/${blobId.name}`,
+      url: `${prefix}/${blobId.driveId}/${blobId.type}/${blobId.variant}/${blobId.name}`,
     })
 
     t.is(res.statusCode, 200, 'request successful')
@@ -47,26 +35,14 @@ test('Plugin handles prefix option properly', async (t) => {
 solo(
   'Unsupported blob type and variant params are handled properly',
   async (t) => {
-    const { blobStore, server } = await testenv()
+    const { blobStore, data, server } = await testenv()
 
     server.register(BlobServerPlugin, { blobStore })
 
-    for (const fixture of IMAGE_FIXTURES) {
-      const imagePath = path.resolve(IMAGE_FIXTURES_PATH, fixture)
-      const parsedFixture = path.parse(fixture)
-      const diskbuf = await readFile(imagePath)
-
-      const blobId = /** @type {const} */ ({
-        type: 'photo',
-        variant: 'original',
-        name: parsedFixture.name,
-      })
-
-      const driveId = await blobStore.put(blobId, diskbuf)
-
+    for (const { blobId } of data) {
       const unsupportedVariantRes = await server.inject({
         method: 'GET',
-        url: `/${driveId}/${blobId.type}/foo/${blobId.name}`,
+        url: `/${blobId.driveId}/${blobId.type}/foo/${blobId.name}`,
       })
 
       t.is(unsupportedVariantRes.statusCode, 400)
@@ -74,7 +50,7 @@ solo(
 
       const unsupportedTypeRes = await server.inject({
         method: 'GET',
-        url: `/${driveId}/foo/${blobId.variant}/${blobId.name}`,
+        url: `/${blobId.driveId}/foo/${blobId.variant}/${blobId.name}`,
       })
 
       t.is(unsupportedTypeRes.statusCode, 400)
@@ -84,82 +60,48 @@ solo(
 )
 
 test('GET photo returns correct blob payload', async (t) => {
-  const { blobStore, server } = await testenv()
+  const { blobStore, data, server } = await testenv()
 
   server.register(BlobServerPlugin, { blobStore })
 
-  for (const fixture of IMAGE_FIXTURES) {
-    const imagePath = path.resolve(IMAGE_FIXTURES_PATH, fixture)
-    const parsedFixture = path.parse(fixture)
-    const diskbuf = await readFile(imagePath)
-
-    const blobId = /** @type {const} */ ({
-      type: 'photo',
-      variant: 'original',
-      name: parsedFixture.name,
-    })
-
-    const driveId = await blobStore.put(blobId, diskbuf)
-
+  for (const { blobId, image } of data) {
     const res = await server.inject({
       method: 'GET',
-      url: `/${driveId}/${blobId.type}/${blobId.variant}/${blobId.name}`,
+      url: `/${blobId.driveId}/${blobId.type}/${blobId.variant}/${blobId.name}`,
     })
 
-    t.alike(res.rawPayload, diskbuf, 'should be equal')
+    t.alike(res.rawPayload, image.data, 'should be equal')
   }
 })
 
 test('GET photo returns inferred content header if metadata is not found', async (t) => {
-  const { blobStore, server } = await testenv()
+  const { blobStore, data, server } = await testenv()
 
   server.register(BlobServerPlugin, { blobStore })
 
-  for (const fixture of IMAGE_FIXTURES) {
-    const imagePath = path.resolve(IMAGE_FIXTURES_PATH, fixture)
-    const parsedFixture = path.parse(fixture)
-    const diskbuf = await readFile(imagePath)
-
-    const blobId = /** @type {const} */ ({
-      type: 'photo',
-      variant: 'original',
-      name: parsedFixture.name,
-    })
-
-    const driveId = await blobStore.put(blobId, diskbuf)
-
+  for (const { blobId, image } of data) {
     const res = await server.inject({
       method: 'GET',
-      url: `/${driveId}/${blobId.type}/${blobId.variant}/${blobId.name}`,
+      url: `/${blobId.driveId}/${blobId.type}/${blobId.variant}/${blobId.name}`,
     })
 
     const expectedContentHeader =
-      getImageMimeType(parsedFixture.ext) || 'application/octet-stream'
+      getImageMimeType(image.ext) || 'application/octet-stream'
 
     t.is(res.headers['content-type'], expectedContentHeader, 'should be equal')
   }
 })
 
 test('GET photo uses mime type from metadata if found', async (t) => {
-  const { blobStore, server } = await testenv()
+  const { blobStore, data, server } = await testenv()
 
   server.register(BlobServerPlugin, { blobStore })
 
-  for (const fixture of IMAGE_FIXTURES) {
-    const imagePath = path.resolve(IMAGE_FIXTURES_PATH, fixture)
-    const parsedFixture = path.parse(fixture)
-    const diskbuf = await readFile(imagePath)
-
-    const blobId = /** @type {const} */ ({
-      type: 'photo',
-      variant: 'original',
-      name: parsedFixture.name,
-    })
-
-    const imageMimeType = getImageMimeType(parsedFixture.ext)
+  for (const { blobId, image } of data) {
+    const imageMimeType = getImageMimeType(image.ext)
     const metadata = imageMimeType ? { mime: imageMimeType } : undefined
 
-    const driveId = await blobStore.put(blobId, diskbuf, {
+    const driveId = await blobStore.put(blobId, image.data, {
       metadata: imageMimeType ? { mime: imageMimeType } : undefined,
     })
 
@@ -179,9 +121,39 @@ test('GET photo uses mime type from metadata if found', async (t) => {
 async function testenv(opts) {
   const coreManager = createCoreManager(opts)
   const blobStore = new BlobStore({ coreManager })
+  const data = await populateStore(blobStore)
   const server = Fastify()
 
-  return { blobStore, coreManager, server }
+  return { blobStore, data, coreManager, server }
+}
+
+/**
+ * @param {import('../lib/blob-store').BlobStore} blobStore
+ */
+async function populateStore(blobStore) {
+  /** @type {{blobId: import('../lib/types').BlobId, image: {data: Buffer, ext: string}}[]} */
+  const data = []
+
+  for (const fixture of IMAGE_FIXTURES) {
+    const imagePath = path.resolve(IMAGE_FIXTURES_PATH, fixture)
+    const parsedFixture = path.parse(fixture)
+    const diskBuffer = await readFile(imagePath)
+
+    const blobIdBase = /** @type {const} */ ({
+      type: 'photo',
+      variant: 'original',
+      name: parsedFixture.name,
+    })
+
+    const driveId = await blobStore.put(blobIdBase, diskBuffer)
+
+    data.push({
+      blobId: { ...blobIdBase, driveId },
+      image: { data: diskBuffer, ext: parsedFixture.ext },
+    })
+  }
+
+  return data
 }
 
 function getImageMimeType(extension) {
