cf0b5350da7a7019062c666100c25e892ee24424
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-08-08 15:49:19 +0100

    Add MapeoProject class and update e2e tests

--- test-e2e/crud.js
+++ test-e2e/crud.js
@@ -1,15 +1,7 @@
-import Database from 'better-sqlite3'
-import { drizzle } from 'drizzle-orm/better-sqlite3'
-import { temporaryFile } from 'tempy'
-import fs from 'node:fs'
-import { execSync } from 'child_process'
 import { test } from 'brittle'
-import RAM from 'random-access-memory'
-import { DataStore } from '../lib/datastore/data-store-new.js'
-import { DataType } from '../lib/datatype/data-type-new.js'
-import { IndexWriter } from '../lib/index-writer.js'
-import { observationTable } from '../lib/schema/project.js'
-import { createCoreManager } from '../tests/helpers/core-manager.js'
+import { randomBytes } from 'crypto'
+import { KeyManager } from '@mapeo/crypto'
+import { MapeoProject } from '../lib/mapeo-project.js'
 
 /** @type {import('@mapeo/schema').ObservationValue} */
 const obsValue = {
@@ -21,22 +13,22 @@ const obsValue = {
 }
 
 test('create and read', async (t) => {
-  const observation = await createDataType(t)
-  const written = await observation.create(obsValue)
-  const read = await observation.getByDocId(written.docId)
+  const project = await createProject()
+  const written = await project.observation.create(obsValue)
+  const read = await project.observation.getByDocId(written.docId)
   t.alike(written, read)
 })
 
 test('update', async (t) => {
-  const observation = await createDataType(t)
-  const written = await observation.create(obsValue)
+  const project = await createProject()
+  const written = await project.observation.create(obsValue)
   const writtenValue = valueOf(written)
-  const updated = await observation.update(written.versionId, {
+  const updated = await project.observation.update(written.versionId, {
     ...writtenValue,
     lon: 0.573453,
     lat: 50.854259,
   })
-  const updatedReRead = await observation.getByDocId(written.docId)
+  const updatedReRead = await project.observation.getByDocId(written.docId)
   t.alike(updated, updatedReRead)
   // Floating-point errors
   t.ok((updated.lon || 0) - 0.573453 < 0.000001)
@@ -44,7 +36,7 @@ test('update', async (t) => {
 })
 
 test('getMany', async (t) => {
-  const observation = await createDataType(t)
+  const project = await createProject()
   const obs = new Array(5).fill(null).map((value, index) => {
     return {
       ...obsValue,
@@ -52,11 +44,11 @@ test('getMany', async (t) => {
     }
   })
   for (const value of obs) {
-    await observation.create(value)
+    await project.observation.create(value)
   }
-  const many = await observation.getMany()
+  const many = await project.observation.getMany()
   const manyValues = many.map((doc) => valueOf(doc))
-  t.alike(manyValues, obs)
+  t.alike(stripUndef(manyValues), obs)
 })
 
 /**
@@ -70,44 +62,21 @@ function valueOf(doc) {
   return rest
 }
 
-/** @param {import('brittle').TestInstance} t */
-async function createDataType(t) {
-  const coreManager = createCoreManager()
-  await coreManager.getWriterCore('auth').core.ready()
-  const { db, sqlite } = createDb(t)
-  const indexWriter = new IndexWriter({
-    tables: [observationTable],
-    sqlite,
-  })
-  const dataStore = new DataStore({
-    coreManager,
-    namespace: 'data',
-    indexWriter,
-    storage: () => new RAM(),
-  })
-  return new DataType({
-    dataStore,
-    table: observationTable,
-    db,
-    getPermissions: () => {},
+function createProject({
+  rootKey = randomBytes(16),
+  projectKey = randomBytes(32),
+} = {}) {
+  const keyManager = new KeyManager(rootKey)
+  return new MapeoProject({
+    keyManager,
+    projectKey,
   })
 }
 
-/** @param {import('brittle').TestInstance} t */
-function createDb(t) {
-  const dbPath = temporaryFile()
-
-  execSync(
-    `drizzle-kit push:sqlite --schema=lib/schema/project.js --driver=better-sqlite --url=${dbPath}`
-  )
-
-  const sqlite = new Database(dbPath)
-  const db = drizzle(sqlite)
-
-  t.teardown(() => {
-    sqlite.close()
-    fs.unlinkSync(dbPath)
-  })
-
-  return { db, sqlite }
+/**
+ * Remove undefined properties from an object, to allow deep comparison
+ * @param {object} obj
+ */
+function stripUndef(obj) {
+  return JSON.parse(JSON.stringify(obj))
 }
