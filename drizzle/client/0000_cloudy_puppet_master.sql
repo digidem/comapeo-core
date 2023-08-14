eab89d87e556af4ecbfe457d8d960565ff31c77d
Andrew Chou <andrewchou@fastmail.com>
2023-08-14 11:28:14 -0400

    add drizzle migration files for client

---
+++ drizzle/client/0000_cloudy_puppet_master.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `project_backlink` (
+	`versionId` text PRIMARY KEY NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `project` (
+	`docId` text PRIMARY KEY NOT NULL,
+	`versionId` text NOT NULL,
+	`schemaName` text NOT NULL,
+	`createdAt` text NOT NULL,
+	`updatedAt` text NOT NULL,
+	`links` text NOT NULL,
+	`name` text NOT NULL,
+	`defaultPresets` text NOT NULL,
+	`forks` text NOT NULL
+);
