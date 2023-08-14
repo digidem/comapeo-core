8dfa18c25ef29562fc11c4b4d017aa38b0674c9e
Andrew Chou <andrewchou@fastmail.com>
2023-08-14 16:17:35 -0400

    chore: regenerate client db files with drizzle

--- drizzle/client/0000_unknown_umar.sql
+++ drizzle/client/0000_unknown_umar.sql
@@ -2,6 +2,16 @@ CREATE TABLE `project_backlink` (
 	`versionId` text PRIMARY KEY NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE `projectKeys` (
+	`projectId` text PRIMARY KEY NOT NULL,
+	`projectSecretKey` blob,
+	`0EncryptionKey` blob,
+	`1EncryptionKey` blob,
+	`2EncryptionKey` blob,
+	`3EncryptionKey` blob,
+	`randomEncryptionKey` blob
+);
+--> statement-breakpoint
 CREATE TABLE `project` (
 	`docId` text PRIMARY KEY NOT NULL,
 	`versionId` text NOT NULL,
