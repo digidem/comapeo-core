3530ba2fb1d0799162daf23129af0e8e8c88ad9f
Andrew Chou <andrewchou@fastmail.com>
2023-08-14 11:10:38 -0400

    feat: add schema definition for project keys table

--- src/schema/client.js
+++ src/schema/client.js
@@ -1,10 +1,23 @@
 // These schemas are all in a "client" database. There is only one client
 // database and it contains information that is shared across all projects on a
 // device
-import { sqliteTable } from 'drizzle-orm/sqlite-core'
+import { blob, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { dereferencedDocSchemas as schemas } from '@mapeo/schema'
 import { jsonSchemaToDrizzleColumns as toColumns } from './schema-to-drizzle.js'
 import { backlinkTable } from './utils.js'
 
-export const projectTable = sqliteTable('project', toColumns(schemas.project))
-export const projectBacklinkTable = backlinkTable(projectTable)
+export const projectInfoTable = sqliteTable(
+  'project',
+  toColumns(schemas.project)
+)
+
+export const projectKeysTable = sqliteTable('project_keys', {
+  projectKey: text('projectKey').notNull().primaryKey(),
+  projectSecretKey: blob('projectSecretKey'),
+  authEncryptionKey: blob('authEncryptionKey'),
+  dataEncryptionKey: blob('dataEncryptionKey'),
+  blobIndexEncryptionKey: blob('blobIndexEncryptionKey'),
+  blobEncryptionKey: blob('blobEncryptionKey'),
+})
+
+export const projectBacklinkTable = backlinkTable(projectInfoTable)
