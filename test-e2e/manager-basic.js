17be4d78c63a95d8cba4cab913c1d6cea610ac5f
Andrew Chou <andrewchou@fastmail.com>
2023-08-21 16:05:51 -0400

    add basic test

---
+++ test-e2e/manager-basic.js
@@ -0,0 +1,24 @@
+import { test } from 'brittle'
+import { MapeoManager } from '../src/mapeo-manager.js'
+
+test('MapeoManager.createProject works', async (t) => {
+  const manager = new MapeoManager()
+
+  const expectedSettings = {
+    name: 'foo',
+  }
+
+  const projectId = await manager.createProject({ name: expectedSettings.name })
+
+  t.ok(projectId)
+
+  const project = manager.getProject(projectId)
+
+  const settings = await project.$getProjectSettings()
+
+  t.is(
+    settings.name,
+    expectedSettings.name,
+    'settings for fetched project are the same as when created'
+  )
+})
