26e9310710e28daff6099157482f61bb66f4761b
Andrew Chou <andrewchou@fastmail.com>
2023-08-23 09:33:29 -0400

    Remove test
    This is more unit-test like and really is testing the
    Project.#handleConfigEntries method.
    
    In a follow-up, we should extract that method to a normal function and
    explicitly unit-test that instead.

--- test-e2e/project-settings.js
+++ test-e2e/project-settings.js
@@ -39,53 +39,6 @@ const fixtures = [
   },
 ]
 
-test('Project settings indexer only indexes project record types', async (t) => {
-  /** @type {string[]} */
-  const allProjectVersionIds = []
-
-  /** @type {string[]} */
-  const indexedProjectVersionIds = []
-
-  const shared = setupSharedResources()
-
-  const originalBatch = shared.indexWriter.batch
-
-  // Hook into the batch method for testing purposes
-  shared.indexWriter.batch = async (entries) => {
-    for (const { index, key } of entries) {
-      const versionId = getVersionId({ coreKey: key, index })
-      indexedProjectVersionIds.push(versionId)
-    }
-
-    return originalBatch.call(shared.indexWriter, entries)
-  }
-
-  const project = createProject({
-    sharedDb: shared.db,
-    sharedIndexWriter: shared.indexWriter,
-  })
-
-  for (const value of fixtures) {
-    const { schemaName } = value
-
-    if (schemaName === 'project') {
-      const written = await project.$setProjectSettings(value)
-      allProjectVersionIds.push(written.versionId)
-    } else {
-      // @ts-expect-error - TS can't figure this out, but we're not testing types here so ok to ignore
-      await project[schemaName].create(value)
-    }
-  }
-
-  t.is(indexedProjectVersionIds.length, allProjectVersionIds.length)
-
-  t.ok(
-    allProjectVersionIds.every((id) => {
-      return indexedProjectVersionIds.includes(id)
-    })
-  )
-})
-
 test('Project settings indexer works across multiple projects', async (t) => {
   /** @type {string[]} */
   const projectVersionIds1 = []
