96e90eab1ae11e10c52f23643969fdf1b49bc906
Andrew Chou <andrewchou@fastmail.com>
2023-08-23 13:44:37 -0400

    update settings test to be more e2e
    currently fails because of some issue when attempting to update the
    settings.

--- test-e2e/project-settings.js
+++ test-e2e/project-settings.js
@@ -1,18 +1,33 @@
 import { test } from 'brittle'
-import { setupSharedResources, createProject } from './utils.js'
+import { MapeoManager } from '../src/mapeo-manager.js'
+import { MapeoProject } from '../src/mapeo-project.js'
+import { removeUndefinedFields } from './utils.js'
 
 test('Project settings create, read, and update operations', async (t) => {
-  const shared = setupSharedResources()
+  const manager = new MapeoManager()
 
-  const project = createProject({
-    sharedDb: shared.db,
-    sharedIndexWriter: shared.indexWriter,
-  })
+  const projectId = await manager.createProject()
+
+  console.log('PROJECT CREATED')
+
+  t.ok(
+    projectId && typeof projectId === 'string',
+    'probably valid project ID returned when creating project'
+  )
+
+  const project = await manager.getProject(projectId)
+
+  t.ok(
+    project instanceof MapeoProject,
+    'manager.getProject() returns MapeoProject instance'
+  )
+
+  const initialSettings = await project.$getProjectSettings()
 
   t.alike(
-    await project.$getProjectSettings(),
+    removeUndefinedFields(initialSettings),
     {},
-    'no settings when project initially created'
+    'project has no settings when initially created'
   )
 
   const expectedSettings = {
@@ -21,13 +36,13 @@ test('Project settings create, read, and update operations', async (t) => {
 
   const updatedSettings = await project.$setProjectSettings(expectedSettings)
 
-  t.is(updatedSettings.name, expectedSettings.name, 'updatable fields change')
+  t.is(updatedSettings.name, expectedSettings.name, 'updatable settings change')
 
   const settings = await project.$getProjectSettings()
 
   t.alike(
-    updatedSettings,
     settings,
+    updatedSettings,
     'retrieved settings are equivalent to most recently updated'
   )
 })
--- test-e2e/utils.js
+++ test-e2e/utils.js
@@ -47,3 +47,11 @@ export function createProject({
     sharedIndexWriter: sharedIndexWriter,
   })
 }
+
+/**
+ * Lazy way of removing fields with undefined values from an object
+ * @param {unknown} object
+ */
+export function removeUndefinedFields(object) {
+  return JSON.parse(JSON.stringify(object))
+}
