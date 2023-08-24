aa426177fa8e1321f6d514209a3911fb408a99ad
Andrew Chou <andrewchou@fastmail.com>
2023-08-24 09:52:53 -0400

    add projectInfo column to projectKeys table

--- src/schema/client.js
+++ src/schema/client.js
@@ -4,11 +4,12 @@
 import { blob, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { dereferencedDocSchemas as schemas } from '@mapeo/schema'
 import { jsonSchemaToDrizzleColumns as toColumns } from './schema-to-drizzle.js'
-import { backlinkTable } from './utils.js'
+import { backlinkTable, customJson } from './utils.js'
 
 export const projectTable = sqliteTable('project', toColumns(schemas.project))
 export const projectBacklinkTable = backlinkTable(projectTable)
 export const projectKeysTable = sqliteTable('projectKeys', {
   projectId: text('projectId').notNull().primaryKey(),
   keysCipher: blob('keysCipher', { mode: 'buffer' }).notNull(),
+  projectInfo: customJson('projectInfo').default(JSON.stringify({})),
 })
