dbc52c35aedc9fcaeb59e8fbc93d05244b46a562
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-08-30 15:17:53 +0100

    feat: handle `coreOwnership` records in `IndexWriter` [1/3] (#214)
    WIP add coreOwnership
    
    custom getWinner and initial mapDoc
    
    Add verifyCoreOwnership function
    
    Add mapAndValidateCoreOwnership
    
    move some code around
    
    add mapDoc and getWinner options to indexWriter
    
    Add tests & small fix
    
    Fix types and use defaultGetWinner from @mapeo/sqlite-indexer
    
    WIP add coreOwnership
    
    custom getWinner and initial mapDoc
    
    Add verifyCoreOwnership function
    
    Add mapAndValidateCoreOwnership
    
    move some code around
    
    add mapDoc and getWinner options to indexWriter
    
    Add tests & small fix
    
    Fix types and use defaultGetWinner from @mapeo/sqlite-indexer

--- src/index-writer/index.js
+++ src/index-writer/index.js
@@ -9,6 +9,9 @@ import { getBacklinkTableName } from '../schema/utils.js'
 /**
  * @typedef {import('@mapeo/schema').MapeoDoc} MapeoDoc
  */
+/**
+ * @typedef {ReturnType<import('@mapeo/schema').decode>} MapeoDocInternal
+ */
 
 /**
  * @template {MapeoDocTables} [TTables=MapeoDocTables]
@@ -16,13 +19,17 @@ import { getBacklinkTableName } from '../schema/utils.js'
 export class IndexWriter {
   /** @type {Map<TTables['_']['name'], SqliteIndexer>} */
   #indexers = new Map()
+  #mapDoc
   /**
    *
    * @param {object} opts
    * @param {import('better-sqlite3').Database} opts.sqlite
    * @param {TTables[]} opts.tables
+   * @param {(doc: MapeoDocInternal, version: import('@mapeo/schema').VersionIdObject) => MapeoDoc} [opts.mapDoc] optionally transform a document prior to indexing. Can also validate, if an error is thrown then the document will not be indexed
+   * @param {typeof import('@mapeo/sqlite-indexer').defaultGetWinner} [opts.getWinner] custom function to determine the "winner" of two forked documents. Defaults to choosing the document with the most recent `updatedAt`
    */
-  constructor({ tables, sqlite }) {
+  constructor({ tables, sqlite, mapDoc = (d) => d, getWinner }) {
+    this.#mapDoc = mapDoc
     for (const table of tables) {
       const config = getTableConfig(table)
       const schemaName = /** @type {(typeof table)['_']['name']} */ (
@@ -31,6 +38,7 @@ export class IndexWriter {
       const indexer = new SqliteIndexer(sqlite, {
         docTableName: config.name,
         backlinkTableName: getBacklinkTableName(config.name),
+        getWinner,
       })
       this.#indexers.set(schemaName, indexer)
     }
@@ -51,9 +59,10 @@ export class IndexWriter {
     const queued = {}
     for (const { block, key, index } of entries) {
       try {
-        var doc = decode(block, { coreKey: key, index })
+        const version = { coreKey: key, index }
+        var doc = this.#mapDoc(decode(block, version), version)
       } catch (e) {
-        // Unknown entry - silently ignore
+        // Unknown or invalid entry - silently ignore
         continue
       }
       if (queued[doc.schemaName]) {
