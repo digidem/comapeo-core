9011e1a0ed864fd690ba091e9bb60f009870e8f7
sethvincent <sethvincent@gmail.com>
2023-08-31 15:29:27 -0700

    speculative isMember and addProject function usage

--- src/invite-api.js
+++ src/invite-api.js
@@ -5,30 +5,42 @@ import { idToKey, keyToId } from './utils.js'
 /** @typedef {import('./rpc/index.js').MapeoRPC} MapeoRPC */
 
 export class InviteApi extends TypedEmitter {
-  // TODO: are invites persisted beyond this api?
   #invites = new Map()
+  #keys = new Map()
+
+  #isMember
+  #addProject
 
   /**
    * @param {Object} options
    * @param {MapeoRPC} options.rpc
+   * @param {object} options.queries
+   * @param {(projectId: string) => Promise<boolean>} options.queries.isMember
+   * @param {(projectId: string, encryptionKeys: import('./types.js').KeyPair) => Promise<void>} options.queries.addProject
    */
-  constructor({ rpc }) {
+  constructor({ rpc, queries }) {
     super()
     this.rpc = rpc
+    this.#isMember = queries.isMember
+    this.#addProject = queries.addProject
 
-    // TODO: I'm not seeing encryption keys used in the inviteResponse in the rpc api
-    // what is the purpose of the encryption keys at this stage of the process or afterward?
-    this.rpc.on('invite', (peerId, invite) => {
+    this.rpc.on('invite', async (peerId, invite) => {
       const projectId = keyToId(invite.projectKey)
-      const peerIds = this.#invites.get(projectId) || new Set()
-      peerIds.add(peerId)
-      this.#invites.set(projectId, peerIds)
 
-      if (peerIds.size === 1) {
-        this.emit('invite-received', {
-          projectId,
-          peerId,
-        })
+      if (await this.#isMember(projectId)) {
+        this.alreadyJoined(projectId)
+      } else {
+        const peerIds = this.#invites.get(projectId) || new Set()
+        peerIds.add(peerId)
+        this.#invites.set(projectId, peerIds)
+        this.#keys.set(projectId, invite.encryptionKeys)
+  
+        if (peerIds.size === 1) {
+          this.emit('invite-received', {
+            projectId,
+            peerId,
+          })
+        }
       }
     })
   }
@@ -48,22 +60,65 @@ export class InviteApi extends TypedEmitter {
   }
 
   /**
+   * @param {string} projectId 
+   */
+  alreadyJoined(projectId) {
+    this.#respond({ projectId, decision: InviteResponse_Decision.ALREADY })
+  }
+
+  /**
    * @param {Object} options
    * @param {string} options.projectId
    * @param {InviteResponse_Decision} options.decision
    */
   #respond({ projectId, decision }) {
-    const peerIds = this.#getPeerIds(projectId)
+    const peerIds = Array.from(this.#getPeerIds(projectId))
+    const encryptionKeys = this.#keys.get(projectId)
     const projectKey = idToKey(projectId)
 
-    // TODO: should this reply to  one peer with `ACCEPT` and the rest with `ALREADY`?
-    // How is the `ALREADY` decision determined? It looks like that isn't used yet.
-    // Does anything bad happen if we respond to multiple peers with `ACCEPT`?
-    for (const peerId of peerIds.values()) {
-      this.rpc.inviteResponse(peerId, { projectKey, decision })
+    let connectedPeerId
+    let remainingPeerIds = []
+
+    for (const peerId of peerIds) {
+      if (!connectedPeerId && this.#isPeerConnected(peerId)) {
+        connectedPeerId = peerId
+      } else if (this.#isPeerConnected(peerId)) {
+        remainingPeerIds.push(peerId)
+      }
+    }
+
+    if (!connectedPeerId) {
+      throw new Error('No connected peer to respond to')
+    }
+
+    this.rpc.inviteResponse(connectedPeerId, { projectKey, decision })
+
+    if (decision === InviteResponse_Decision.ACCEPT) {
+      this.#addProject(projectId, encryptionKeys)
+
+      for (const peerId of remainingPeerIds) {
+        this.alreadyJoined(projectId)
+      }
+    } else if (decision === InviteResponse_Decision.REJECT) {
+      for (const peerId of remainingPeerIds) {
+        this.reject(projectId)
+      }
+    } else if (decision === InviteResponse_Decision.ALREADY) {
+      for (const peerId of remainingPeerIds) {
+        this.alreadyJoined(projectId)
+      }
     }
 
     this.#invites.delete(projectId)
+    this.#keys.delete(projectId)
+  }
+
+  /**
+   * @param {string} peerId 
+   * @returns {boolean}
+   */
+  #isPeerConnected(peerId) {
+    return this.rpc.peers.some((peer) => peer.id === peerId)
   }
 
   /**
