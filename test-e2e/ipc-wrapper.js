d8b0996aecd15222921f5b9d874e5d49cf077881
Andrew Chou <andrewchou@fastmail.com>
2023-09-12 13:25:21 -0400

    add test

---
+++ test-e2e/ipc-wrapper.js
@@ -0,0 +1,42 @@
+import { test } from 'brittle'
+import { MessageChannel } from 'node:worker_threads'
+import RAM from 'random-access-memory'
+import { KeyManager } from '@mapeo/crypto'
+import { createMapeoServer } from '../src/ipc-wrapper/server.js'
+import { MapeoManager } from '../src/mapeo-manager.js'
+import { createClient } from 'rpc-reflector'
+import { createMapeoClient } from '../src/ipc-wrapper/client.js'
+
+test('IPC wrappers work', async (t) => {
+  const { port1, port2 } = new MessageChannel()
+
+  const manager = new MapeoManager({
+    rootKey: KeyManager.generateRootKey(),
+    dbFolder: ':memory:',
+    coreStorage: () => new RAM(),
+  })
+
+  const server = createMapeoServer(manager, port1)
+  const client = createMapeoClient(port2)
+
+  port1.start()
+  port2.start()
+
+  const projectId = await client.createProject({ name: 'mapeo' })
+
+  t.ok(projectId)
+
+  const project = await client.getProject(projectId)
+
+  t.ok(project)
+
+  const projectSettings = await project.$getProjectSettings()
+
+  t.alike(projectSettings, { name: 'mapeo' })
+
+  server.close()
+  createClient.close(client)
+
+  port1.close()
+  port2.close()
+})
