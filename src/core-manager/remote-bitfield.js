4042a8fde1e6428c2605d5707b1c5f3499aed9ad
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-09-13 16:24:55 +0100

    feat: send core "haves" bitfield on first connect (#254)

---
+++ src/core-manager/remote-bitfield.js
@@ -0,0 +1,390 @@
+// Thanks to:
+// https://github.com/holepunchto/hypercore/blob/c572826/lib/remote-bitfield.js
+// With added JSDoc types
+
+import BigSparseArray from 'big-sparse-array'
+import { quickbit } from './compat.js'
+
+const BITS_PER_PAGE = 32768
+const BYTES_PER_PAGE = BITS_PER_PAGE / 8
+const WORDS_PER_PAGE = BYTES_PER_PAGE / 4
+const BITS_PER_SEGMENT = 2097152
+const BYTES_PER_SEGMENT = BITS_PER_SEGMENT / 8
+const PAGES_PER_SEGMENT = BITS_PER_SEGMENT / BITS_PER_PAGE
+
+class RemoteBitfieldPage {
+  /**
+   *
+   * @param {number} index
+   * @param {Uint32Array} bitfield
+   * @param {RemoteBitfieldSegment} segment
+   */
+  constructor(index, bitfield, segment) {
+    /** @type {typeof index} */
+    this.index = index
+    /** @type {number} */
+    this.offset = index * BYTES_PER_PAGE - segment.offset
+    /** @type {typeof bitfield} */
+    this.bitfield = bitfield
+    /** @type {typeof segment} */
+    this.segment = segment
+
+    segment.add(this)
+  }
+
+  get tree() {
+    return this.segment.tree
+  }
+
+  /**
+   *
+   * @param {number} index
+   * @returns
+   */
+  get(index) {
+    return quickbit.get(this.bitfield, index)
+  }
+
+  /**
+   *
+   * @param {number} index
+   * @param {boolean} val
+   */
+  set(index, val) {
+    if (quickbit.set(this.bitfield, index, val)) {
+      this.tree.update(this.offset * 8 + index)
+    }
+  }
+
+  /**
+   *
+   * @param {number} start
+   * @param {number} length
+   * @param {boolean} val
+   */
+  setRange(start, length, val) {
+    quickbit.fill(this.bitfield, val, start, start + length)
+
+    let i = Math.floor(start / 32)
+    const n = i + Math.ceil(length / 32)
+
+    while (i < n) this.tree.update(this.offset * 8 + i++ * 32)
+  }
+  /**
+   *
+   * @param {boolean} val
+   * @param {number} position
+   */
+  findFirst(val, position) {
+    return quickbit.findFirst(this.bitfield, val, position)
+  }
+  /**
+   * @param {boolean} val
+   * @param {number} position
+   */
+  findLast(val, position) {
+    return quickbit.findLast(this.bitfield, val, position)
+  }
+
+  /**
+   *
+   * @param {number} start
+   * @param {Uint32Array} bitfield
+   */
+  insert(start, bitfield) {
+    this.bitfield.set(bitfield, start / 32)
+  }
+}
+
+class RemoteBitfieldSegment {
+  /**
+   *
+   * @param {number} index
+   */
+  constructor(index) {
+    this.index = index
+    this.offset = index * BYTES_PER_SEGMENT
+    this.tree = /** @type {import('quickbit-universal').SparseIndex} */ (
+      quickbit.Index.from([], BYTES_PER_SEGMENT)
+    )
+    this.pages = new Array(PAGES_PER_SEGMENT)
+  }
+
+  get chunks() {
+    return this.tree.chunks
+  }
+
+  /**
+   * @param {RemoteBitfieldPage} page
+   */
+  add(page) {
+    this.pages[page.index - this.index * PAGES_PER_SEGMENT] = page
+
+    const chunk = { field: page.bitfield, offset: page.offset }
+
+    this.chunks.push(chunk)
+
+    for (let i = this.chunks.length - 2; i >= 0; i--) {
+      const prev = this.chunks[i]
+      if (prev.offset <= chunk.offset) break
+      this.chunks[i] = chunk
+      this.chunks[i + 1] = prev
+    }
+  }
+
+  /**
+   *
+   * @param {boolean} val
+   * @param {number} position
+   */
+  findFirst(val, position) {
+    position = this.tree.skipFirst(!val, position)
+
+    let j = position & (BITS_PER_PAGE - 1)
+    let i = (position - j) / BITS_PER_PAGE
+
+    if (i >= PAGES_PER_SEGMENT) return -1
+
+    while (i < this.pages.length) {
+      const p = this.pages[i]
+
+      let index = -1
+
+      if (p) index = p.findFirst(val, j)
+      else if (!val) index = j
+
+      if (index !== -1) return i * BITS_PER_PAGE + index
+
+      j = 0
+      i++
+    }
+
+    return -1
+  }
+
+  /**
+   * @param {boolean} val
+   * @param {number} position
+   */
+  findLast(val, position) {
+    position = this.tree.skipLast(!val, position)
+
+    let j = position & (BITS_PER_PAGE - 1)
+    let i = (position - j) / BITS_PER_PAGE
+
+    if (i >= PAGES_PER_SEGMENT) return -1
+
+    while (i >= 0) {
+      const p = this.pages[i]
+
+      let index = -1
+
+      if (p) index = p.findLast(val, j)
+      else if (!val) index = j
+
+      if (index !== -1) return i * BITS_PER_PAGE + index
+
+      j = BITS_PER_PAGE - 1
+      i--
+    }
+
+    return -1
+  }
+}
+
+export default class RemoteBitfield {
+  constructor() {
+    /** @type {BigSparseArray<RemoteBitfieldPage>} */
+    this._pages = new BigSparseArray()
+    /** @type {BigSparseArray<RemoteBitfieldSegment>} */
+    this._segments = new BigSparseArray()
+  }
+
+  /**
+   * @param {number} index
+   */
+  get(index) {
+    const j = index & (BITS_PER_PAGE - 1)
+    const i = (index - j) / BITS_PER_PAGE
+
+    const p = this._pages.get(i)
+
+    return p ? p.get(j) : false
+  }
+
+  /**
+   * @param {number} index
+   * @param {boolean} val
+   */
+  set(index, val) {
+    const j = index & (BITS_PER_PAGE - 1)
+    const i = (index - j) / BITS_PER_PAGE
+
+    let p = this._pages.get(i)
+
+    if (!p && val) {
+      const k = Math.floor(i / PAGES_PER_SEGMENT)
+      const s =
+        this._segments.get(k) ||
+        this._segments.set(k, new RemoteBitfieldSegment(k))
+
+      p = this._pages.set(
+        i,
+        new RemoteBitfieldPage(i, new Uint32Array(WORDS_PER_PAGE), s)
+      )
+    }
+
+    if (p) p.set(j, val)
+  }
+
+  /**
+   * @param {number} start
+   * @param {number} length
+   * @param {boolean} val
+   */
+  setRange(start, length, val) {
+    let j = start & (BITS_PER_PAGE - 1)
+    let i = (start - j) / BITS_PER_PAGE
+
+    while (length > 0) {
+      let p = this._pages.get(i)
+
+      if (!p && val) {
+        const k = Math.floor(i / PAGES_PER_SEGMENT)
+        const s =
+          this._segments.get(k) ||
+          this._segments.set(k, new RemoteBitfieldSegment(k))
+
+        p = this._pages.set(
+          i,
+          new RemoteBitfieldPage(i, new Uint32Array(WORDS_PER_PAGE), s)
+        )
+      }
+
+      const end = Math.min(j + length, BITS_PER_PAGE)
+      const range = end - j
+
+      if (p) p.setRange(j, range, val)
+
+      j = 0
+      i++
+      length -= range
+    }
+  }
+
+  /**
+   * @param {boolean} val
+   * @param {number} position
+   */
+  findFirst(val, position) {
+    let j = position & (BITS_PER_SEGMENT - 1)
+    let i = (position - j) / BITS_PER_SEGMENT
+
+    while (i < this._segments.maxLength) {
+      const s = this._segments.get(i)
+
+      let index = -1
+
+      if (s) index = s.findFirst(val, j)
+      else if (!val) index = j
+
+      if (index !== -1) return i * BITS_PER_SEGMENT + index
+
+      j = 0
+      i++
+    }
+
+    return val ? -1 : position
+  }
+  /**
+   * @param {number} position
+   */
+  firstSet(position) {
+    return this.findFirst(true, position)
+  }
+  /**
+   * @param {number} position
+   */
+  firstUnset(position) {
+    return this.findFirst(false, position)
+  }
+  /**
+   * @param {boolean} val
+   * @param {number} position
+   */
+  findLast(val, position) {
+    let j = position & (BITS_PER_SEGMENT - 1)
+    let i = (position - j) / BITS_PER_SEGMENT
+
+    while (i >= 0) {
+      const s = this._segments.get(i)
+
+      let index = -1
+
+      if (s) index = s.findLast(val, j)
+      else if (!val) index = j
+
+      if (index !== -1) return i * BITS_PER_SEGMENT + index
+
+      j = BITS_PER_SEGMENT - 1
+      i--
+    }
+
+    return -1
+  }
+
+  /**
+   * @param {number} position
+   */
+  lastSet(position) {
+    return this.findLast(true, position)
+  }
+  /**
+   * @param {number} position
+   */
+  lastUnset(position) {
+    return this.findLast(false, position)
+  }
+  /**
+   * @param {number} start
+   * @param {Uint32Array} bitfield
+   * @returns
+   */
+  insert(start, bitfield) {
+    if (start % 32 !== 0) return false
+
+    let length = bitfield.byteLength * 8
+
+    let j = start & (BITS_PER_PAGE - 1)
+    let i = (start - j) / BITS_PER_PAGE
+
+    while (length > 0) {
+      let p = this._pages.get(i)
+
+      if (!p) {
+        const k = Math.floor(i / PAGES_PER_SEGMENT)
+        const s =
+          this._segments.get(k) ||
+          this._segments.set(k, new RemoteBitfieldSegment(k))
+
+        p = this._pages.set(
+          i,
+          new RemoteBitfieldPage(i, new Uint32Array(WORDS_PER_PAGE), s)
+        )
+      }
+
+      const end = Math.min(j + length, BITS_PER_PAGE)
+      const range = end - j
+
+      p.insert(j, bitfield.subarray(0, range / 32))
+
+      bitfield = bitfield.subarray(range / 32)
+
+      j = 0
+      i++
+      length -= range
+    }
+
+    return true
+  }
+}
---
+++ tests/bitfield-rle.js
@@ -0,0 +1,126 @@
+import test from 'brittle'
+import Bitfield from 'bitfield'
+import * as rle from '../src/core-manager/bitfield-rle.js'
+
+test('encodes and decodes', function (t) {
+  var bits = new Bitfield(1024)
+  var deflated = rle.encode(toUint32Array(bits.buffer))
+  t.ok(deflated.length < bits.buffer.length, 'is smaller')
+  var inflated = rle.decode(deflated)
+  t.alike(inflated, toUint32Array(bits.buffer), 'decodes to same buffer')
+  t.end()
+})
+
+test('encodingLength', function (t) {
+  var bits = new Bitfield(1024)
+  var len = rle.encodingLength(bits.buffer)
+  t.ok(len < bits.buffer.length, 'is smaller')
+  var deflated = rle.encode(bits.buffer)
+  t.alike(
+    len,
+    deflated.length,
+    'encoding length is similar to encoded buffers length'
+  )
+  t.end()
+})
+
+test('encodes and decodes with all bits set', function (t) {
+  var bits = new Bitfield(1024)
+
+  for (var i = 0; i < 1024; i++) bits.set(i, true)
+
+  var deflated = rle.encode(toUint32Array(bits.buffer))
+  t.ok(deflated.length < bits.buffer.length, 'is smaller')
+  var inflated = rle.decode(deflated)
+  t.alike(inflated, toUint32Array(bits.buffer), 'decodes to same buffer')
+  t.end()
+})
+
+test('encodes and decodes with some bits set', function (t) {
+  var bits = new Bitfield(1024)
+
+  bits.set(500, true)
+  bits.set(501, true)
+  bits.set(502, true)
+
+  bits.set(999, true)
+  bits.set(1000, true)
+  bits.set(0, true)
+
+  var deflated = rle.encode(toUint32Array(bits.buffer))
+  t.ok(deflated.length < bits.buffer.length, 'is smaller')
+  var inflated = rle.decode(deflated)
+  t.alike(inflated, toUint32Array(bits.buffer), 'decodes to same buffer')
+  t.end()
+})
+
+test('encodes and decodes with random bits set', function (t) {
+  var bits = new Bitfield(8 * 1024)
+
+  for (var i = 0; i < 512; i++) {
+    bits.set(Math.floor(Math.random() * 8 * 1024), true)
+  }
+
+  var deflated = rle.encode(toUint32Array(bits.buffer))
+  t.ok(deflated.length < bits.buffer.length, 'is smaller')
+  var inflated = rle.decode(deflated)
+  t.alike(inflated, toUint32Array(bits.buffer), 'decodes to same buffer')
+  t.end()
+})
+
+test('encodes and decodes with random bits set (not power of two)', function (t) {
+  var bits = new Bitfield(8 * 1024)
+
+  for (var i = 0; i < 313; i++) {
+    bits.set(Math.floor(Math.random() * 8 * 1024), true)
+  }
+
+  var deflated = rle.encode(toUint32Array(bits.buffer))
+  t.ok(deflated.length < bits.buffer.length, 'is smaller')
+  var inflated = rle.decode(deflated)
+  t.alike(inflated, toUint32Array(bits.buffer), 'decodes to same buffer')
+  t.end()
+})
+
+test('encodes empty bitfield', function (t) {
+  var deflated = rle.encode(new Uint32Array())
+  var inflated = rle.decode(deflated)
+  t.alike(inflated, new Uint32Array(), 'still empty')
+  t.end()
+})
+
+test('throws on bad input', function (t) {
+  t.exception(function () {
+    rle.decode(toUint32Array([100, 0, 0, 0]))
+  }, 'invalid delta count')
+  // t.exception.all also catches RangeErrors, which is what we expect from this
+  t.exception.all(function () {
+    rle.decode(
+      toUint32Array([
+        10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0,
+        10, 0,
+      ])
+    )
+  }, 'missing delta')
+  t.end()
+})
+
+test('not power of two', function (t) {
+  var deflated = rle.encode(toUint32Array([255, 255, 255, 240]))
+  var inflated = rle.decode(deflated)
+  t.alike(
+    inflated,
+    toUint32Array([255, 255, 255, 240]),
+    'output equal to input'
+  )
+  t.end()
+})
+
+/** @param {Bitfield | Buffer | Array<number>} b */
+function toUint32Array(b) {
+  if (Array.isArray(b)) {
+    b = Buffer.from(b)
+  }
+  const buf = b instanceof Bitfield ? b.buffer : b
+  return new Uint32Array(buf.buffer, buf.byteOffset, buf.byteLength / 4)
+}
