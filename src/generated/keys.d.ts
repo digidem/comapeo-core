7142b862c7863aa79a2fa403624fc9d64b92025a
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-09-14 16:16:52 +0100

    feat: share all core keys via extension messages (#264)
    * fix findPeer bug (also in #254)
    
    * feat: share all core keys via extension messages
    
    Fixes #251
    
    This adds new fields to the project extension to share keys for other
    namespaces and updates tests which manually added cores (manual adding
    is no longer needed, but do need to wait for cores to be added)

--- src/generated/keys.d.ts
+++ src/generated/keys.d.ts
@@ -15,8 +15,88 @@ export interface ProjectKeys {
 export declare const EncryptionKeys: {
     encode(message: EncryptionKeys, writer?: _m0.Writer): _m0.Writer;
     decode(input: _m0.Reader | Uint8Array, length?: number): EncryptionKeys;
+    create<I extends {
+        auth?: Buffer;
+        data?: Buffer | undefined;
+        config?: Buffer | undefined;
+        blobIndex?: Buffer | undefined;
+        blob?: Buffer | undefined;
+    } & {
+        auth?: Buffer;
+        data?: Buffer | undefined;
+        config?: Buffer | undefined;
+        blobIndex?: Buffer | undefined;
+        blob?: Buffer | undefined;
+    } & { [K in Exclude<keyof I, keyof EncryptionKeys>]: never; }>(base?: I): EncryptionKeys;
+    fromPartial<I_1 extends {
+        auth?: Buffer;
+        data?: Buffer | undefined;
+        config?: Buffer | undefined;
+        blobIndex?: Buffer | undefined;
+        blob?: Buffer | undefined;
+    } & {
+        auth?: Buffer;
+        data?: Buffer | undefined;
+        config?: Buffer | undefined;
+        blobIndex?: Buffer | undefined;
+        blob?: Buffer | undefined;
+    } & { [K_1 in Exclude<keyof I_1, keyof EncryptionKeys>]: never; }>(object: I_1): EncryptionKeys;
 };
 export declare const ProjectKeys: {
     encode(message: ProjectKeys, writer?: _m0.Writer): _m0.Writer;
     decode(input: _m0.Reader | Uint8Array, length?: number): ProjectKeys;
+    create<I extends {
+        projectKey?: Buffer;
+        projectSecretKey?: Buffer | undefined;
+        encryptionKeys?: {
+            auth?: Buffer;
+            data?: Buffer | undefined;
+            config?: Buffer | undefined;
+            blobIndex?: Buffer | undefined;
+            blob?: Buffer | undefined;
+        };
+    } & {
+        projectKey?: Buffer;
+        projectSecretKey?: Buffer | undefined;
+        encryptionKeys?: {
+            auth?: Buffer;
+            data?: Buffer | undefined;
+            config?: Buffer | undefined;
+            blobIndex?: Buffer | undefined;
+            blob?: Buffer | undefined;
+        } & {
+            auth?: Buffer;
+            data?: Buffer | undefined;
+            config?: Buffer | undefined;
+            blobIndex?: Buffer | undefined;
+            blob?: Buffer | undefined;
+        } & { [K in Exclude<keyof I["encryptionKeys"], keyof EncryptionKeys>]: never; };
+    } & { [K_1 in Exclude<keyof I, keyof ProjectKeys>]: never; }>(base?: I): ProjectKeys;
+    fromPartial<I_1 extends {
+        projectKey?: Buffer;
+        projectSecretKey?: Buffer | undefined;
+        encryptionKeys?: {
+            auth?: Buffer;
+            data?: Buffer | undefined;
+            config?: Buffer | undefined;
+            blobIndex?: Buffer | undefined;
+            blob?: Buffer | undefined;
+        };
+    } & {
+        projectKey?: Buffer;
+        projectSecretKey?: Buffer | undefined;
+        encryptionKeys?: {
+            auth?: Buffer;
+            data?: Buffer | undefined;
+            config?: Buffer | undefined;
+            blobIndex?: Buffer | undefined;
+            blob?: Buffer | undefined;
+        } & {
+            auth?: Buffer;
+            data?: Buffer | undefined;
+            config?: Buffer | undefined;
+            blobIndex?: Buffer | undefined;
+            blob?: Buffer | undefined;
+        } & { [K_2 in Exclude<keyof I_1["encryptionKeys"], keyof EncryptionKeys>]: never; };
+    } & { [K_3 in Exclude<keyof I_1, keyof ProjectKeys>]: never; }>(object: I_1): ProjectKeys;
 };
