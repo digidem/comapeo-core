5c3f473a5a06fdf004c41bd7f34f7ae3901bd28a
Andrew Chou <andrewchou@fastmail.com>
2023-09-20 15:31:28 -0400

    refactor respond method to not be recursive

--- src/invite-api.js
+++ src/invite-api.js
@@ -44,8 +44,11 @@ export class InviteApi extends TypedEmitter {
       peerIds.add(peerId)
       this.#peersToRespondTo.set(projectId, peerIds)
 
-      if (await this.#isMember(projectId)) {
-        this.alreadyJoined(projectId)
+      const isAlreadyMember = await this.#isMember(projectId)
+
+      if (isAlreadyMember) {
+        // TODO: Catch the error and no-op here?
+        this.#respond({ projectId, decision: InviteResponse_Decision.ALREADY })
       } else {
         this.#invites.set(projectId, invite)
 
@@ -64,23 +67,35 @@ export class InviteApi extends TypedEmitter {
 
   /**
    * @param {string} projectId
+   * @returns {Promise<void>}
    */
-  accept(projectId) {
-    this.#respond({ projectId, decision: InviteResponse_Decision.ACCEPT })
+  async accept(projectId) {
+    const isAlreadyMember = await this.#isMember(projectId)
+
+    // TODO: Is this check necessary?
+    // If so, do we want to respond here or throw an error?
+    if (isAlreadyMember) {
+      return this.#respond({
+        projectId,
+        decision: InviteResponse_Decision.ALREADY,
+      })
+    }
+
+    return this.#respond({
+      projectId,
+      decision: InviteResponse_Decision.ACCEPT,
+    })
   }
 
   /**
    * @param {string} projectId
+   * @returns {Promise<void>}
    */
-  reject(projectId) {
-    this.#respond({ projectId, decision: InviteResponse_Decision.REJECT })
-  }
-
-  /**
-   * @param {string} projectId
-   */
-  alreadyJoined(projectId) {
-    this.#respond({ projectId, decision: InviteResponse_Decision.ALREADY })
+  async reject(projectId) {
+    return this.#respond({
+      projectId,
+      decision: InviteResponse_Decision.REJECT,
+    })
   }
 
   /**
@@ -88,56 +103,75 @@ export class InviteApi extends TypedEmitter {
    * @param {string} options.projectId
    * @param {InviteResponse_Decision} options.decision
    */
-  #respond({ projectId, decision }) {
-    const peerIds = this.#getPeerIds(projectId)
+  async #respond({ projectId, decision }) {
     const projectKey = Buffer.from(projectId, 'hex')
 
-    let connectedPeerId
-    let remainingPeerIds = new Set()
+    switch (decision) {
+      case InviteResponse_Decision.ACCEPT: {
+        const invite = this.#invites.get(projectId)
 
-    for (const peerId of peerIds) {
-      if (!connectedPeerId && this.#isPeerConnected(peerId)) {
-        connectedPeerId = peerId
-      } else if (this.#isPeerConnected(peerId)) {
-        remainingPeerIds.add(peerId)
+        if (!invite) {
+          throw new Error(
+            `Cannot find invite for project with ID ${projectKeyToPublicId(
+              projectKey
+            )}`
+          )
+        }
+
+        // TODO: What should we do if this throws?
+        await this.#addProject(invite)
+
+        const connectedPeers = this.#getConnectedProjectPeers(projectId)
+
+        // TODO: Is this what we want to do here?
+        if (connectedPeers.size === 0) {
+          throw new Error('No connected peers to respond to')
+        }
+
+        const [firstPeerId, ...remainingPeerIds] = Array.from(connectedPeers)
+
+        this.rpc.inviteResponse(firstPeerId, {
+          projectKey,
+          decision: InviteResponse_Decision.ACCEPT,
+        })
+
+        // Respond to the remaining peers with ALREADY
+        for (const peerId of remainingPeerIds) {
+          this.rpc.inviteResponse(peerId, {
+            projectKey,
+            decision: InviteResponse_Decision.ALREADY,
+          })
+        }
       }
-    }
+      case InviteResponse_Decision.REJECT: {
+        const connectedPeers = this.#getConnectedProjectPeers(projectId)
 
-    if (!connectedPeerId) {
-      throw new Error('No connected peer to respond to')
-    }
+        // TODO: Is this what we want to do here?
+        if (connectedPeers.size === 0) {
+          throw new Error('No connected peers to respond to')
+        }
 
-    this.rpc.inviteResponse(connectedPeerId, {
-      projectKey,
-      decision,
-    })
-
-    if (decision === InviteResponse_Decision.ACCEPT) {
-      const invite = this.#invites.get(projectId)
-
-      if (!invite) {
-        throw new Error(
-          `Cannot find invite for project with ID ${projectKeyToPublicId(
-            projectKey
-          )}`
-        )
+        for (const peerId of connectedPeers) {
+          this.rpc.inviteResponse(peerId, {
+            projectKey,
+            decision: InviteResponse_Decision.REJECT,
+          })
+        }
       }
+      case InviteResponse_Decision.ALREADY: {
+        const connectedPeers = this.#getConnectedProjectPeers(projectId)
 
-      this.#addProject(invite)
+        // TODO: Is this what we want to do here?
+        if (connectedPeers.size === 0) {
+          throw new Error('No connected peers to respond to')
+        }
 
-      // eslint-disable-next-line no-unused-vars
-      for (const _peerId of remainingPeerIds) {
-        this.alreadyJoined(projectId)
-      }
-    } else if (decision === InviteResponse_Decision.REJECT) {
-      // eslint-disable-next-line no-unused-vars
-      for (const _peerId of remainingPeerIds) {
-        this.reject(projectId)
-      }
-    } else if (decision === InviteResponse_Decision.ALREADY) {
-      // eslint-disable-next-line no-unused-vars
-      for (const _peerId of remainingPeerIds) {
-        this.alreadyJoined(projectId)
+        for (const peerId of connectedPeers) {
+          this.rpc.inviteResponse(peerId, {
+            projectKey,
+            decision: InviteResponse_Decision.ALREADY,
+          })
+        }
       }
     }
 
@@ -157,7 +191,25 @@ export class InviteApi extends TypedEmitter {
    * @param {string} projectId
    * @returns {Set<string>} peerIds
    */
-  #getPeerIds(projectId) {
+  #getProjectPeerIds(projectId) {
     return this.#peersToRespondTo.get(projectId) || new Set()
+  }
+
+  /**
+   *
+   * @param {string} projectId
+   * @returns {Set<string>}
+   */
+  #getConnectedProjectPeers(projectId) {
+    const connected = new Set()
+    const projectPeerIds = this.#getProjectPeerIds(projectId)
+
+    for (const id of projectPeerIds) {
+      if (this.#isPeerConnected(id)) {
+        connected.add(id)
+      }
+    }
+
+    return connected
   }
 }
--- tests/invite-api.js
+++ tests/invite-api.js
@@ -6,7 +6,7 @@ import { InviteApi } from '../src/invite-api.js'
 import { replicate } from './helpers/rpc.js'
 
 test('Accept invite', async (t) => {
-  t.plan(3)
+  t.plan(4)
 
   const { rpc: r1, projectKey, encryptionKeys } = setup()
 
@@ -37,17 +37,19 @@ test('Accept invite', async (t) => {
     t.is(response, MapeoRPC.InviteResponse.ACCEPT)
   })
 
-  inviteApi.on('invite-received', ({ projectId }) => {
+  inviteApi.on('invite-received', async ({ projectId }) => {
     t.is(projectId, projectKey.toString('hex'))
 
-    inviteApi.accept(projectId)
+    await inviteApi.accept(projectId)
+
+    t.ok(projects.has(projectId), 'project successfully added')
   })
 
   replicate(r1, r2)
 })
 
 test('Reject invite', async (t) => {
-  t.plan(3)
+  t.plan(4)
 
   const { rpc: r1, projectKey, encryptionKeys } = setup()
 
@@ -78,59 +80,112 @@ test('Reject invite', async (t) => {
     t.is(response, MapeoRPC.InviteResponse.REJECT)
   })
 
-  inviteApi.on('invite-received', ({ projectId }) => {
+  inviteApi.on('invite-received', async ({ projectId }) => {
     t.is(projectId, projectKey.toString('hex'))
 
-    inviteApi.reject(projectId)
+    await inviteApi.reject(projectId)
+
+    t.is(projects.has(projectId), false, 'project not added')
   })
 
   replicate(r1, r2)
 })
 
 test('Receiving invite for project that peer already belongs to', async (t) => {
-  t.plan(2)
+  t.test('was member prior to connection', async (t) => {
+    t.plan(2)
 
-  const { rpc: r1, projectKey, encryptionKeys } = setup()
+    const { rpc: r1, projectKey, encryptionKeys } = setup()
 
-  // Start off being already part of the project
-  const projects = new Map([
-    [projectKey.toString('hex'), { projectKey, encryptionKeys }],
-  ])
+    // Start off being already part of the project
+    const projects = new Map([
+      [projectKey.toString('hex'), { projectKey, encryptionKeys }],
+    ])
 
-  const r2 = new MapeoRPC()
+    const r2 = new MapeoRPC()
 
-  const inviteApi = new InviteApi({
-    rpc: r2,
-    queries: {
-      isMember: async (projectId) => {
-        return projects.has(projectId)
+    const inviteApi = new InviteApi({
+      rpc: r2,
+      queries: {
+        isMember: async (projectId) => {
+          return projects.has(projectId)
+        },
+        addProject: async (invite) => {
+          projects.set(invite.projectKey.toString('hex'), invite)
+        },
       },
-      addProject: async (invite) => {
-        projects.set(invite.projectKey.toString('hex'), invite)
-      },
-    },
-  })
-
-  r1.on('peers', async (peers) => {
-    t.is(peers.length, 1)
-
-    const response = await r1.invite(peers[0].id, {
-      projectKey,
-      encryptionKeys,
     })
 
-    t.is(
-      response,
-      MapeoRPC.InviteResponse.ALREADY,
-      'invited peer automatically responds with "ALREADY"'
-    )
+    r1.on('peers', async (peers) => {
+      t.is(peers.length, 1)
+
+      const response = await r1.invite(peers[0].id, {
+        projectKey,
+        encryptionKeys,
+      })
+
+      t.is(
+        response,
+        MapeoRPC.InviteResponse.ALREADY,
+        'invited peer automatically responds with "ALREADY"'
+      )
+    })
+
+    inviteApi.on('invite-received', () => {
+      t.fail('invite-received event should not have been emitted')
+    })
+
+    replicate(r1, r2)
   })
 
-  inviteApi.on('invite-received', () => {
-    t.fail('invite-received event should not have been emitted')
-  })
+  t.test('became member from accepting prior invite', async (t) => {
+    t.plan(3)
 
-  replicate(r1, r2)
+    const { rpc: r1, projectKey, encryptionKeys } = setup()
+
+    const projects = new Map()
+
+    const r2 = new MapeoRPC()
+
+    const inviteApi = new InviteApi({
+      rpc: r2,
+      queries: {
+        isMember: async (projectId) => {
+          return projects.has(projectId)
+        },
+        addProject: async (invite) => {
+          projects.set(invite.projectKey.toString('hex'), invite)
+        },
+      },
+    })
+
+    r1.on('peers', async (peers) => {
+      const response1 = await r1.invite(peers[0].id, {
+        projectKey,
+        encryptionKeys,
+      })
+
+      t.is(response1, MapeoRPC.InviteResponse.ACCEPT)
+
+      const response2 = await r1.invite(peers[0].id, {
+        projectKey,
+        encryptionKeys,
+      })
+
+      t.is(response2, MapeoRPC.InviteResponse.ALREADY)
+    })
+
+    let inviteReceivedEventCount = 0
+
+    inviteApi.on('invite-received', ({ projectId }) => {
+      inviteReceivedEventCount += 1
+      t.is(inviteReceivedEventCount, 1)
+
+      inviteApi.accept(projectId)
+    })
+
+    replicate(r1, r2)
+  })
 })
 
 function setup() {
