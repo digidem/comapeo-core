cbee37111e6769007aebba0e2845df182aa568d3
Tomás Ciccola <tciccola@digital-democracy.com>
2023-10-09 15:09:01 -0300

    Trying to make tests work
    It seems that when calling dataStore.write (wrapped on iconApi.create),
    the method returns the doc by doing `this.getDocById(docId)`. This seems
    to return nothing (which is weird, if `.write` is succesfull, shoudn't
    `.getDocById` return something??
    ```bash
    file:///home/szgy/data/src/dd/mapeo-core-next/src/datatype/index.js:140
        if (!result) throw new Error('Not found')
                           ^
    
    Error: Not found
        at DataType.getByDocId (file:///home/szgy/data/src/dd/mapeo-core-next/src/datatype/index.js:140:24)
        at DataType.[kCreateWithDocId] (file:///home/szgy/data/src/dd/mapeo-core-next/src/datatype/index.js:132:17)
        at async IconApi.create (file:///home/szgy/data/src/dd/mapeo-core-next/src/icon-api.js:41:12)
        at async file:///home/szgy/data/src/dd/mapeo-core-next/tests/icon-api.js:36:19
        at async Test._run (/home/szgy/data/src/dd/mapeo-core-next/node_modules/brittle/index.js:573:7)
    ```

--- tests/icon-api.js
+++ tests/icon-api.js
@@ -2,6 +2,7 @@ import test from 'brittle'
 import RAM from 'random-access-memory'
 import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
 
 import IconApi from '../src/icon-api.js'
 import { DataType } from '../src/datatype/index.js'
@@ -14,6 +15,10 @@ test('icon create and get', async (t) => {
   const cm = createCoreManager()
   const sqlite = new Database(':memory:')
   const db = drizzle(sqlite)
+  migrate(db, {
+    migrationsFolder: new URL('../drizzle/project', import.meta.url).pathname,
+  })
+
   const iconDataStore = new DataStore({
     namespace: 'config',
     coreManager: cm,
@@ -28,11 +33,14 @@ test('icon create and get', async (t) => {
     db,
   })
   const iconApi = new IconApi({ iconDataStore, iconDataType })
-  const iconDoc = iconApi.create({
+  const iconDoc = await iconApi.create({
     name: 'myIcon',
     schemaName: 'icon',
     variants: [
       {
+        size: 'small',
+        pixelDensity: 1,
+        mimeType: 'image/png',
         blob: Buffer.from('myIcon'),
       },
     ],
