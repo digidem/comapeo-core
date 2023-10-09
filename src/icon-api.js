1fb8482ca10a7490bc4189e01c1aaf86fa590418
Tomás Ciccola <tciccola@digital-democracy.com>
2023-10-09 13:21:48 -0300

    add IconApi class

---
+++ src/icon-api.js
@@ -0,0 +1,71 @@
+import { parseVersionId } from '@mapeo/schema'
+
+export class IconApi {
+  #dataType
+  #dataStore
+
+  /**
+   * @param {Object} opts
+   * @param {import('./datatype/index.js').DataType<
+   *   import('./datastore/index.js').DataStore<'config'>,
+   *   typeof import('./schema/project.js').iconTable,
+   *   'icon',
+   *   import('@mapeo/schema').Icon,
+   *   import('@mapeo/schema').IconValue
+   * >} opts.iconDataType
+   * @param {import('./datastore/index.js').DataStore<'config'>} opts.iconDataStore
+   */
+  constructor({ iconDataType, iconDataStore }) {
+    this.#dataType = iconDataType
+    this.#dataStore = iconDataStore
+  }
+
+  /**
+   * @param {Omit<import('@mapeo/schema').IconValue, 'variants'> &
+   * {variants: Array<
+   *    import('@mapeo/schema/dist/proto/icon/v1.js').Icon_1_IconVariant
+   *    & {blob: Buffer}>}} icon
+   */
+  async create(icon) {
+    const blobsIds = await Promise.all(
+      icon.variants.map(({ blob }) => {
+        return this.#dataStore.writeRaw(blob)
+      })
+    )
+    const variantsWithId = icon.variants.map((variant, i) => {
+      return { ...variant, blobVersionId: blobsIds[i] }
+    })
+    return await this.#dataType.create({
+      schemaName: 'icon',
+      name: icon.name,
+      variants: variantsWithId,
+    })
+  }
+
+  /**
+   * @param {Object} opts
+   * @param {String} opts.iconId
+   * @param {String} opts.size
+   * @param {number} opts.pixelDensity
+   */
+  async getIcon({ iconId, size, pixelDensity }) {
+    const iconRecord = await this.#dataType.getByDocId(iconId)
+    const iconVariant = this.#getBestVariant(iconRecord.variants, {
+      size,
+      pixelDensity,
+    })
+    return await this.#dataStore.readRaw(iconVariant.blobVersionId)
+  }
+
+  /**
+   * @param {import('@mapeo/schema').IconValue['variants']} variants
+   * @param {object} opts
+   * @param {string} opts.size
+   * @param {number} opts.pixelDensity
+   **/
+  #getBestVariant(variants, { size, pixelDensity }) {
+    console.log(size, pixelDensity)
+    // TODO
+    return variants[0]
+  }
+}
