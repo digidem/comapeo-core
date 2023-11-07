cc61a9ba707507b97fa39eae2466b0ab2d1e913e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-11-07 14:11:37 +0900

    Add failing test

--- test-e2e/media-server.js
+++ test-e2e/media-server.js
@@ -2,6 +2,8 @@ import { test } from 'brittle'
 import { join } from 'path'
 import { fileURLToPath } from 'url'
 import { KeyManager } from '@mapeo/crypto'
+import { fetch } from 'undici'
+import fs from 'fs/promises'
 import RAM from 'random-access-memory'
 
 import { MapeoManager } from '../src/mapeo-manager.js'
@@ -44,6 +46,13 @@ test('retrieving blobs urls', async (t) => {
     new URL(blobUrl),
     'retrieving url based on media server resolves after starting it'
   )
+
+  const response = await fetch(blobUrl)
+  t.is(response.status, 200)
+  t.is(response.headers.get('content-type'), 'image/png')
+  const expected = await fs.readFile(join(BLOB_FIXTURES_DIR, 'original.png'))
+  const body = Buffer.from(await response.arrayBuffer())
+  t.alike(body, expected)
 
   await manager.stop()
 
