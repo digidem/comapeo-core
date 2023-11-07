3af655b3b5cab9652c1b210b0514db402a102567
Andrew Chou <andrewchou@fastmail.com>
2023-11-07 14:31:41 -0500

    update plugin implementation
    changes the following:
    
    - specifies `getProject()` opt for plugin instead of using fastify decorator
    - updates url params matching to account for updated icon url format

--- src/icon-server/fastify-plugin.js
+++ src/icon-server/fastify-plugin.js
@@ -1,6 +1,8 @@
 import { Type as T } from '@sinclair/typebox'
 import fp from 'fastify-plugin'
-import { kIconApi } from '../mapeo-project.js'
+import { docSchemas } from '@mapeo/schema'
+
+import { kGetIconBlob } from '../icon-api.js'
 
 export default fp(iconServerPlugin, {
   fastify: '4.x',
@@ -12,53 +14,92 @@ const HEX_REGEX_32_BYTES = '^[0-9a-fA-F]{64}$'
 // projectId is encoded to a z-base-32 52-character string (32 bytes)
 const Z_BASE_32_REGEX_32_BYTES = '^[0-9a-zA-Z]{52}$'
 const ICON_DOC_ID_STRING = T.String({ pattern: HEX_REGEX_32_BYTES })
-const PROJECT_ID_STRING = T.String({ pattern: Z_BASE_32_REGEX_32_BYTES })
+const PROJECT_PUBLIC_ID_STRING = T.String({ pattern: Z_BASE_32_REGEX_32_BYTES })
+
+const sizes = docSchemas.icon.properties.variants.items.properties.size.enum
+const mimeTypes =
+  docSchemas.icon.properties.variants.items.properties.mimeType.enum
 
 const PARAMS_JSON_SCHEMA = T.Object({
   iconDocId: ICON_DOC_ID_STRING,
-  projectId: PROJECT_ID_STRING,
-  size: T.String(),
-  pixelDensity: T.Number(),
+  projectPublicId: PROJECT_PUBLIC_ID_STRING,
+  size: T.Union(sizes.map((size) => T.Literal(size))),
+  pixelDensity: T.Optional(T.String()), // e.g. @2x
+  mimeTypeExtension: T.Union(
+    mimeTypes.map((mimeType) => {
+      switch (mimeType) {
+        case 'image/png':
+          return T.Literal('png')
+        case 'image/svg+xml':
+          return T.Literal('svg')
+      }
+    })
+  ),
 })
 
 /**
  * @typedef {Object} IconServerPluginOpts
- * @property {import('fastify').RegisterOptions['prefix']} prefix
+ *
+ * @property {(projectId: string) => Promise<import('../mapeo-project.js').MapeoProject>} getProject
  **/
 
 /** @type {import('fastify').FastifyPluginAsync<import('fastify').RegisterOptions & IconServerPluginOpts>} */
 async function iconServerPlugin(fastify, options) {
+  if (!options.getProject) throw new Error('Missing getProject')
   fastify.register(routes, options)
 }
 
-/**
- * @typedef {(projectId: string) =>
- * Promise<import('../mapeo-project.js').MapeoProject>} GetProject
- */
-
-/** @type {import('fastify').FastifyPluginAsync<
- * Omit<IconServerPluginOpts, 'prefix'>,
- * import('fastify').RawServerDefault,
- * import('@fastify/type-provider-typebox').TypeBoxTypeProvider>}
- * */
-async function routes(fastify) {
-  const { getProject } = fastify
+/** @type {import('fastify').FastifyPluginAsync<Omit<IconServerPluginOpts, 'prefix'>, import('fastify').RawServerDefault, import('@fastify/type-provider-typebox').TypeBoxTypeProvider>} */
+async function routes(fastify, options) {
+  const { getProject } = options
 
   fastify.get(
-    '/:projectId/:iconDocId/:size/:pixelDensity',
+    '/:projectPublicId/:iconDocId/:size:pixelDensity(@[123]x)?.:mimeTypeExtension',
     { schema: { params: PARAMS_JSON_SCHEMA } },
     async (req, res) => {
-      const { projectId, iconDocId, size, pixelDensity } = req.params
-      const project = await getProject(projectId)
-      const iconApi = project[kIconApi]
-      let icon
+      const { projectPublicId, iconDocId, size, mimeTypeExtension } = req.params
+
+      const pixelDensity = req.params.pixelDensity
+        ? extractPixelDensity(req.params.pixelDensity)
+        : 1
+
+      const project = await getProject(projectPublicId)
+
+      const mimeType =
+        mimeTypeExtension === 'png' ? 'image/png' : 'image/svg+xml'
+
       try {
-        icon = await iconApi.getIcon({ iconId: iconDocId, size, pixelDensity })
-      } catch (e) {
-        return res.code(404)
+        const icon = await project.$icons[kGetIconBlob](iconDocId, {
+          size,
+          pixelDensity,
+          mimeType,
+        })
+
+        res.header('Content-Type', mimeType)
+        return res.send(icon)
+      } catch (err) {
+        res.code(404)
+        throw err
       }
-      res.header('mime-type', icon.mimeType)
-      return res.send(icon.icon)
     }
   )
+}
+
+/**
+ * @param {string} input
+ * @returns {import('@mapeo/schema').Icon['variants'][number]['pixelDensity']}
+ */
+function extractPixelDensity(input) {
+  const result = parseInt(input[1], 10)
+
+  if (isNaN(result)) throw new Error('Could not extract pixel density')
+
+  switch (result) {
+    case 1:
+    case 2:
+    case 3:
+      return result
+    default:
+      throw new Error(`Invalid pixel density ${result}`)
+  }
 }
--- types/fastify.d.ts
+++
@@ -1,7 +0,0 @@
-import { MapeoProject } from '../mapeo-project'
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    getProject: (projectId: string) => Promise<MapeoProject>
-  }
-}
