75ea34aa61d159f4a3d23afe182134e918cd603a
Andrew Chou <andrewchou@fastmail.com>
2023-11-09 02:32:49 -0500

    fix: fix core storage initialization in MapeoManager (#367)
    Co-authored-by: Gregor MacLennan <gmaclennan@digital-democracy.org>

--- src/mapeo-manager.js
+++ src/mapeo-manager.js
@@ -121,7 +121,7 @@ export class MapeoManager extends TypedEmitter {
 
     if (typeof coreStorage === 'string') {
       const pool = new RandomAccessFilePool(MAX_FILE_DESCRIPTORS)
-      // @ts-ignore
+      // @ts-expect-error
       this.#coreStorage = Hypercore.defaultStorage(coreStorage, { pool })
     } else {
       this.#coreStorage = coreStorage
