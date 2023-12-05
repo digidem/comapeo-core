169af6649ae82be7074adc160d09d112ff753838
Andrew Chou <andrewchou@fastmail.com>
2023-12-05 16:05:26 -0500

    const instead of let in close() method

--- src/mapeo-project.js
+++ src/mapeo-project.js
@@ -375,7 +375,7 @@ export class MapeoProject extends TypedEmitter {
   async close() {
     this.#l.log('closing project %h', this.#projectId)
     await this.#coreManager.close()
-    let dataStorePromises = []
+    const dataStorePromises = []
     for (const dataStore of Object.values(this.#dataStores)) {
       dataStorePromises.push(dataStore.close())
     }
