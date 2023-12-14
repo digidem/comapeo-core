aee15c2023e02bf16a84b2369f87e26c5e051448
tomasciccola <117094913+tomasciccola@users.noreply.github.com>
2023-12-14 13:05:07 -0300

    grab existing doc when calling `dataType.delete`, test it (#420)
    * grab existing doc when calling `dataType.delete`, test it
    
    * test value of doc.deleted before and after deletion
    
    * add more assertions on `delete` test
    
    * clean up tests
    
    Co-authored-by: Andrew Chou <andrewchou@fastmail.com>
    
    ---------
    
    Co-authored-by: Tomás Ciccola <tciccola@digital-democracy.com>
    Co-authored-by: Andrew Chou <andrewchou@fastmail.com>

--- tests/data-type.js
+++ tests/data-type.js
@@ -141,6 +141,26 @@ test('test validity of `createdBy` field from another peer', async (t) => {
   await destroy()
 })
 
+test('delete()', async (t) => {
+  const projectKey = randomBytes(32)
+  const { dataType } = await testenv({ projectKey })
+  const doc = await dataType.create(obsFixture)
+  t.is(doc.deleted, false, `'deleted' field is false before deletion`)
+  const deletedDoc = await dataType.delete(doc.versionId)
+  t.is(deletedDoc.deleted, true, `'deleted' field is true after deletion`)
+  t.alike(
+    deletedDoc.links,
+    [doc.versionId],
+    `deleted doc links back to created doc`
+  )
+  const retrievedDocByDocId = await dataType.getByDocId(deletedDoc.docId)
+  t.alike(
+    retrievedDocByDocId,
+    deletedDoc,
+    `retrieving by docId returns deleted doc`
+  )
+})
+
 async function testenv(opts) {
   const coreManager = createCoreManager(opts)
   const sqlite = new Database(':memory:')
