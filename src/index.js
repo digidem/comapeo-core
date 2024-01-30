9d3611102523afc533bb38715257f57a2d53cc48
Andrew Chou <andrewchou@fastmail.com>
2024-01-30 11:13:14 -0500

    export static maps plugin for package

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+export { plugin as MapeoStaticMapsFastifyPlugin } from './fastify-plugins/maps/static-maps.js'
 export { FastifyController } from './fastify-controller.js'
 export { MapeoManager } from './mapeo-manager.js'
