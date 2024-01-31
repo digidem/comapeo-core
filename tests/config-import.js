5836fbdb6b4f6bbc70d70923cf52eb559df7f9e6
Tomás Ciccola <tciccola@digital-democracy.com>
2024-01-31 11:14:08 -0300

    added tests for invalid presets.json file

--- tests/config-import.js
+++ tests/config-import.js
@@ -2,7 +2,7 @@ import { test } from 'brittle'
 import { readConfig } from '../src/config-import.js'
 
 test('reading config file', async (t) => {
-  t.plan(6)
+  // t.plan(7)
   t.exception(async () => await readConfig(''), /ENOENT/, 'file not found')
   t.exception(
     async () => await readConfig('./tests/fixtures/config/notAZip.txt'),
@@ -23,11 +23,46 @@ test('reading config file', async (t) => {
     'missing presets.json'
   )
 
+  t.exception.all(
+    async () =>
+      await readConfig('./tests/fixtures/config/invalidPresetsJSON.zip'),
+    /SyntaxError: Unexpected string in JSON/,
+    'JSON.parse error of presets.json'
+  )
+
   t.exception(
     async () =>
       await readConfig('./tests/fixtures/config/invalidPresetsFile.zip'),
     /Error: Invalid presets.json file/,
     'presets.json is not an object'
+  )
+
+  t.exception(
+    async () =>
+      await readConfig('./tests/fixtures/config/missingPresetsField.zip'),
+    /Error: Invalid presets.json file/,
+    'no presets field in presets.json'
+  )
+
+  t.exception(
+    async () =>
+      await readConfig('./tests/fixtures/config/presetsFieldNotAnObject.zip'),
+    /Error: Invalid presets.json file/,
+    'presets field in presets.json is not an object'
+  )
+
+  t.exception(
+    async () =>
+      await readConfig('./tests/fixtures/config/missingFieldsField.zip'),
+    /Error: Invalid presets.json file/,
+    'no fields field in presets.json'
+  )
+
+  t.exception(
+    async () =>
+      await readConfig('./tests/fixtures/config/fieldsFieldNotAnObject.zip'),
+    /Error: Invalid presets.json file/,
+    'fields field in presets.json is not an object'
   )
 
   t.ok(await readConfig('./tests/fixtures/config/config.zip'), 'valid zip')
