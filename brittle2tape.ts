4566a75e9ff99348603aa7eaac39a4d2079f999c
Evan Hahn <me@evanhahn.com>
2024-02-28 17:13:44 +0000

    Converting exception.all to just exception

--- brittle2tape.ts
+++ brittle2tape.ts
@@ -1,5 +1,13 @@
-import type { JSCodeshift, Transform, Collection } from 'jscodeshift'
+import type {
+  JSCodeshift,
+  Transform,
+  Collection,
+  MemberExpression,
+} from 'jscodeshift'
 
+/**
+ * Convert all `brittle` imports to `tape` imports.
+ */
 const rewriteImports = (j: JSCodeshift, root: Collection<any>) => {
   root
     .find(j.ImportDeclaration, {
@@ -14,6 +22,18 @@ const rewriteImports = (j: JSCodeshift, root: Collection<any>) => {
     })
 }
 
+/**
+ * Rewrite member expressions like `obj.prop`.
+ *
+ * Given `rewriteMemberExpressions(j, root, "foo", "bar")`, produces a diff like:
+ *
+ * ```diff
+ * -one.foo
+ * -two.foo(1, 2, 3)
+ * +one.bar
+ * +two.bar(1, 2, 3)
+ * ```
+ */
 const rewriteMemberExpressions = (
   j: JSCodeshift,
   root: Collection<any>,
@@ -27,6 +47,37 @@ const rewriteMemberExpressions = (
     })
 }
 
+/**
+ * Rewrite `t.exception.all` to `t.exception`.
+ */
+const rewriteExceptionAll = (j: JSCodeshift, root: Collection<any>) => {
+  root
+    .find(j.MemberExpression, {
+      object: {
+        type: 'MemberExpression',
+        property: { name: 'exception' },
+      },
+      property: { name: 'all' },
+    })
+    .forEach((memberExpression) => {
+      memberExpression.value.object = (
+        memberExpression.value.object as MemberExpression
+      ).object
+      memberExpression.value.property = j.identifier('exception')
+    })
+}
+
+/**
+ * Rewrite `t.exception` and `t.exception.all`.
+ *
+ * If passed a synchronous function, do a simple rewrite to `t.throws`.
+ *
+ * If passed anything else, convert to `rejects(t, ...args)` and make sure `rejects` is imported.
+ */
+const rewriteExceptions = (j: JSCodeshift, root: Collection<any>) => {
+  rewriteExceptionAll(j, root)
+}
+
 const transform: Transform = (fileInfo, api) => {
   const { j } = api
   const root = j(fileInfo.source)
@@ -37,6 +88,8 @@ const transform: Transform = (fileInfo, api) => {
   rewriteMemberExpressions(j, root, 'unlike', 'notDeepEqual')
   rewriteMemberExpressions(j, root, 'absent', 'notOk')
   rewriteMemberExpressions(j, root, 'execution', 'doesNotThrow')
+
+  rewriteExceptions(j, root)
 
   return root.toSource()
 }
