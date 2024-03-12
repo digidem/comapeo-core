15c551d337baf11264dbe464a12175bd7e71a8ca
Andrew Chou <andrewchou@fastmail.com>
2024-03-12 14:20:48 -0400

    feat: add functionality to serve offline fallback map (#450)

--- types/fastify.d.ts
+++ types/fastify.d.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 
 import { StaticMapsPluginDecorator } from '../src/fastify-plugins/maps/static-maps.js'
+import { FallbackMapPluginDecorator } from '../src/fastify-plugins/maps/offline-fallback-map.js'
 
 declare module 'fastify' {
   export interface FastifyInstance {
+    mapeoFallbackMap: FallbackMapPluginDecorator
     mapeoStaticMaps: StaticMapsPluginDecorator
   }
 }
