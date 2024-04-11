9f03dfb7c57d280078fb464245b58475a4b52bb3
Tomás Ciccola <tciccola@digital-democracy.com>
2024-04-11 14:34:56 -0300

    updated config script

--- scripts/config.js
+++ scripts/config.js
@@ -1,14 +1,17 @@
 import fs from 'node:fs'
-import path from 'node:path'
 const filename = 'mapeo-default-config'
 const extension = 'mapeoconfig'
-const modPath = 'node_modules/@mapeo/default-config'
-const version = JSON.parse(
-  fs.readFileSync(path.resolve(`${modPath}/package.json`))
-).version
-const configPath = `${modPath}/dist/${filename}-v${version}.${extension}`
+const modPath = '../node_modules/@mapeo/default-config'
+const configPkgJSONPath = new URL(`${modPath}/package.json`, import.meta.url)
+const version = JSON.parse(fs.readFileSync(configPkgJSONPath)).version
 
-fs.copyFileSync(
-  path.resolve(configPath),
-  path.resolve(`tests/fixtures/config/${filename}.${extension}`)
+const configPath = new URL(
+  `${modPath}/dist/${filename}-v${version}.${extension}`,
+  import.meta.url
 )
+const fixturesPath = new URL(
+  `../tests/fixtures/config/${filename}.${extension}`,
+  import.meta.url
+)
+
+fs.copyFileSync(configPath, fixturesPath)
