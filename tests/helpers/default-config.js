b1a4d329142ef8c6843ca99c16e613b06f516a26
Gregor MacLennan <gmaclennan@digital-democracy.org>
2024-04-18 12:18:49 +0100

    update to published config, fix module path

--- tests/helpers/default-config.js
+++ tests/helpers/default-config.js
@@ -1,28 +1,5 @@
 // @ts-check
-import * as fs from 'node:fs'
-import * as path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
 
-const defaultConfigModulePath = path.resolve(
-  fileURLToPath(import.meta.url),
-  '..',
-  '..',
-  '..',
-  'node_modules',
-  '@mapeo',
-  'default-config'
-)
-
-const defaultConfigPkgJsonPath = path.join(
-  defaultConfigModulePath,
-  'package.json'
-)
-const { version } = JSON.parse(
-  fs.readFileSync(defaultConfigPkgJsonPath, 'utf8')
-)
-
-export const defaultConfigPath = path.join(
-  defaultConfigModulePath,
-  'dist',
-  `mapeo-default-config-v${version}.mapeoconfig`
-)
+const require = createRequire(import.meta.url)
+export const defaultConfigPath = require.resolve('@mapeo/default-config')
