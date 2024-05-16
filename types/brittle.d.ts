f9f28f878a63ac5905f93f9d8419a65daa2bb33c
Evan Hahn <me@evanhahn.com>
2024-05-16 18:39:54 +0000

    test: remove `t.plan()` calls
    We plan to replace Brittle with `node:test` which doesn't have an
    equivalent for `t.plan`. Remove it to make it easier to migrate.
    
    Notably, this removes `t.plan` from the type definitions to discourage
    its use.

--- types/brittle.d.ts
+++ types/brittle.d.ts
@@ -64,7 +64,6 @@ declare module 'brittle' {
   }
 
   export interface TestInstance extends Assertion {
-    plan(n: number): void
     teardown(
       fn: () => unknown | Promise<unknown>,
       options?: { order?: number }
@@ -73,6 +72,10 @@ declare module 'brittle' {
     comment(message: string): void
     end(): void
     test: TestFn
+
+    // We plan to replace Brittle with `node:test` which doesn't support `plan`.
+    // Disable it to prevent new code from being written with `t.plan`.
+    // plan(n: number): void
   }
 
   type TestCallback = (t: TestInstance) => void | Promise<void>
