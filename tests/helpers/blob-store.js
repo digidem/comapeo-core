e16bee96e99a1bdb2333a35e10b3028c76c36dfc
Evan Hahn <me@evanhahn.com>
2024-07-30 13:11:27 -0500

    feat: support additional metadata for blobs (#723)
    This adds support for two additional blob metadata fields: location and
    timestamp.
    
    This works when tested against [this CoMapeo PR][0], which sets those
    fields but has to ignore type errors.
    
    Closes [#719].
    
    [0]: https://github.com/digidem/comapeo-mobile/pull/532
    [#719]: https://github.com/digidem/mapeo-core-next/issues/719

--- tests/helpers/blob-store.js
+++ tests/helpers/blob-store.js
@@ -3,6 +3,7 @@ import { pipelinePromise as pipeline, Writable } from 'streamx'
 
 import { BlobStore } from '../../src/blob-store/index.js'
 import { createCoreManager } from './core-manager.js'
+/** @typedef {import('../../src/blob-api.js').Metadata} Metadata */
 
 /**
  * @param {Object} [opts]
@@ -32,3 +33,25 @@ export async function concat(rs) {
   )
   return buf
 }
+
+/**
+ * @param {Partial<Metadata>} overrides
+ * @returns {Metadata}
+ */
+export const blobMetadata = (overrides) => ({
+  mimeType: 'image/png',
+  timestamp: Date.now(),
+  location: {
+    coords: {
+      accuracy: 3,
+      altitude: 8848,
+      altitudeAccuracy: 3,
+      heading: 180,
+      latitude: 27.988333,
+      longitude: 86.925278,
+      speed: 0,
+    },
+    timestamp: Date.now(),
+  },
+  ...overrides,
+})
