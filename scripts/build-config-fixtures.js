f5ff0410bc307748adfbab6285351d7c935de16e
Tomás Ciccola <tciccola@digital-democracy.com>
2024-07-31 14:29:47 -0300

    use `{ withFileTypes: true } ` instead of calling `fs.stat`

--- scripts/build-config-fixtures.js
+++ scripts/build-config-fixtures.js
@@ -1,17 +1,17 @@
 import yazl from 'yazl'
 import * as fs from 'node:fs/promises'
 import { createWriteStream } from 'node:fs'
+import { join } from 'node:path'
 
 const CONFIG_FIXTURES_PATH = new URL(
   '../tests/fixtures/config',
   import.meta.url
 ).pathname
-const dir = await fs.readdir(CONFIG_FIXTURES_PATH)
+const dir = await fs.readdir(CONFIG_FIXTURES_PATH, { withFileTypes: true })
 console.log('zipping config fixtures')
 for (const fileOrFolder of dir) {
-  const p = join(CONFIG_FIXTURES_PATH, fileOrFolder)
-  const stat = await fs.stat(p)
-  if (stat.isDirectory()) {
+  const p = join(CONFIG_FIXTURES_PATH, fileOrFolder.name)
+  if (fileOrFolder.isDirectory()) {
     zipFolder(p)
   }
 }
@@ -26,10 +26,10 @@ async function zipFolder(path) {
   zip.outputStream.pipe(wstream).on('close', function () {
     console.log(`done zipping ${zipPath.split('/').at(-1)}`)
   })
-  const contents = await fs.readdir(path)
+  const contents = await fs.readdir(path, { withFileTypes: true })
   for (const content of contents) {
-    const filePath = `${path}/${content}`
-    for await (const p of walk(filePath)) {
+    const filePath = join(path, content.name)
+    for await (const p of walk(filePath, content.isDirectory())) {
       const zipPath = p.replace(`${path}/`, '')
       zip.addFile(p, zipPath)
     }
@@ -39,15 +39,14 @@ async function zipFolder(path) {
 
 /**
  * @param {string} path
+ * @param {boolean} isDirectory
  * @returns {AsyncGenerator<string>}
  */
- */
-async function* walk(path) {
-  const stat = await fs.stat(path)
-  if (stat.isDirectory()) {
-    const dir = await fs.readdir(path)
+async function* walk(path, isDirectory) {
+  if (isDirectory) {
+    const dir = await fs.readdir(path, { withFileTypes: true })
     for (const newPath of dir) {
-      yield* walk(join(path, newPath))
+      yield* walk(join(path, newPath.name), newPath.isDirectory())
     }
   } else {
     yield path
