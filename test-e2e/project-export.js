956945251a6ac173a352750a538c893e29dd888e
Mauve Signweaver <RangerMauve@hotmail.com>
2025-11-12 09:26:13 -0500

    test: Export on invited peer (#1176)
    * test: Export on invited peer
    
    * test: Use create managers for export peer test
    
    * fix: Get export sync test passing
    
    * chore: Fix export sync test name
    
    * fix: Properly check for blob download during zip export
    
    ---------
    
    Co-authored-by: Mauve Signweaver <contact@mauve.moe>

--- test-e2e/project-export.js
+++ test-e2e/project-export.js
@@ -9,7 +9,13 @@ import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { createReadStream } from 'node:fs'
 
-import { createManager } from './utils.js'
+import {
+  connectPeers,
+  createManager,
+  createManagers,
+  invite,
+  waitForSync,
+} from './utils.js'
 
 /** @import { Readable } from 'streamx' */
 
@@ -215,6 +221,71 @@ test('Project export tracks and observations to zip stream', async (t) => {
   })
 })
 
+test('Sync project and export tracks and observations to zip stream', async (t) => {
+  const managers = await createManagers(2, t, 'device_type_unspecified', {
+    // Mobile is not archive device by default and we should support that.
+    defaultIsArchiveDevice: false,
+  })
+  const [invitor, invitee] = managers
+  const disconnectPeers = connectPeers(managers)
+  t.after(disconnectPeers)
+
+  const { project: invitorProject, projectId } = await setupProject(invitor, {
+    makeTracks: true,
+    makeObservations: true,
+    makeAttachments: true,
+  })
+
+  await invite({
+    invitor,
+    invitees: [invitee],
+    projectId,
+  })
+
+  const project = await invitee.getProject(projectId)
+
+  project.$sync.start({ autostopDataSyncAfter: 10_000 })
+
+  invitorProject.$sync.start({ autostopDataSyncAfter: 10_000 })
+
+  await waitForSync([invitorProject, project], 'full')
+
+  await temporaryDirectoryTask(async (dir) => {
+    const zipFile = await project.exportZipFile(dir, {
+      tracks: true,
+      observations: true,
+      attachments: true,
+    })
+    const zip = new StreamZip.async({ file: zipFile })
+    const entries = Object.keys(await zip.entries())
+
+    const geoJSONFile = entries.find((name) => name.endsWith('.geojson'))
+
+    assert(geoJSONFile, 'Zip file contains geojson file')
+
+    const stream = await zip.stream(geoJSONFile)
+    const parsed = await parseGeoJSON(stream)
+    await zip.close()
+    assert.equal(
+      parsed.type,
+      'FeatureCollection',
+      'Exported GeoJSON is a FeatureCollection'
+    )
+    assert.deepEqual(
+      parsed.features.length,
+      DEFAULT_OBSERVATIONS +
+        DEFAULT_TRACKS +
+        DEFAULT_TRACKS * OBSERVATIONS_PER_TRACK,
+      'Exported GeoJSON has expected number of features'
+    )
+
+    assert.equal(entries.length, 2, 'Zip has geoJSON and one attachment')
+
+    const hasPng = entries.some((name) => name.endsWith('.png'))
+    assert(hasPng, 'Zip has exported PNG')
+  })
+})
+
 /**
  * @param {AsyncIterable<Buffer|Uint8Array|string>} stream
  * @returns {Promise<any>}
@@ -250,7 +321,7 @@ async function setupProject(
   manager,
   { makeObservations = false, makeTracks = false, makeAttachments = false } = {}
 ) {
-  const projectId = await manager.createProject()
+  const projectId = await manager.createProject({ name: 'Export test' })
   const project = await manager.getProject(projectId)
 
   /** @type {import('../src/types.js').Attachment | null} */
@@ -332,5 +403,5 @@ async function setupProject(
     )
   }
 
-  return { project, observations, tracks }
+  return { project, projectId, observations, tracks }
 }
