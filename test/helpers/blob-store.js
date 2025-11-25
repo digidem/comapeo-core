2100ed8fae3af177b4723013be6dbae491f22d55
Mauve Signweaver <contact@mauve.moe>
2025-11-25 11:20:00 -0500

    tests: Clean up resources in blob store utility

--- test/helpers/blob-store.js
+++ test/helpers/blob-store.js
@@ -12,8 +12,13 @@ import { createCoreManager } from './core-manager.js'
  * @param {Buffer} [opts.projectKey]
  */
 export function createBlobStore(t, opts) {
-  const coreManager = createCoreManager(t, opts)
+  const coreManager = createCoreManager(t, opts, false)
   const blobStore = new BlobStore({ coreManager })
+
+  t.after(async () => {
+    await blobStore.close()
+    await coreManager.close()
+  })
   return { blobStore, coreManager }
 }
 
