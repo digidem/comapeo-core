74c84ef7c9c931af37777430a6f854b8af5b612b
Gregor MacLennan <gmaclennan@awana.digital>
2026-05-12 22:17:58 +0100

    fix: remove dangling setTimeout in waitForSync

--- src/sync/sync-api.js
+++ src/sync/sync-api.js
@@ -460,12 +460,13 @@ export class SyncApi extends TypedEmitter {
       /** @param {import('./sync-state.js').State} state */
       const onState = (state) => {
         clearTimeout(timeoutId)
-        if (typeof timeoutMs === 'number') {
-          timeoutId = setTimeout(onTimeout, timeoutMs)
-        }
         if (isSynced(state, type, this.#peerSyncControllers)) {
           this[kSyncState].off('state', onState)
           resolve()
+          return
+        }
+        if (typeof timeoutMs === 'number') {
+          timeoutId = setTimeout(onTimeout, timeoutMs)
         }
       }
       this[kSyncState].on('state', onState)
